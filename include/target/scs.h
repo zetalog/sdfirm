6b17f04918cca3de936f2f4b2f7f919460035e37
Lv Zheng <zetalog@gmail.com>
2012-02-09 10:33:05 +0800

    fix SCS header file definitions.

--- include/target/scs.h
+++ include/target/scs.h
@@ -85,6 +85,15 @@
 #define scs_debug_dump(b)
 #endif
 
+#define SCS_ATR_MAX			33
+#ifdef CONFIG_SCS_APDU_SIZE
+#define SCS_APDU_MAX			CONFIG_SCS_APDU_SIZE
+#else
+#define SCS_APDU_MAX			256
+#endif
+
+typedef uint16_t scs_size_t;
+typedef uint16_t scs_off_t;
 typedef uint8_t scs_err_t;
 
 typedef void (*scs_cmpl_cb)(void);
@@ -111,16 +120,5 @@ typedef void (*scs_tvoid_cb)(void);
 #define SCS_PROTO_T1			0x01
 #define SCS_PROTO_MAX			0x0E
 #define SCD_PROTO_T15			0x0F
-
-#define SCS_ATR_MAX			33
-
-#ifdef CONFIG_SCS_APDU_SIZE
-#define SCS_APDU_MAX			CONFIG_SCS_APDU_SIZE
-#else
-#define SCS_APDU_MAX			256
-#endif
-
-typedef uint16_t scs_size_t;
-typedef uint16_t scs_off_t;
 
 #endif /* __SCS_H_INCLUDE__ */
