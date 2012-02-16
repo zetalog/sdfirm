73f7ea9b65754aaa7f892688d60d731e533f6421
Lv Zheng <zetalog@gmail.com>
2012-02-16 16:24:34 +0800

    add copyright declaration for sdfirm.

--- include/target/muldiv.h
+++ include/target/muldiv.h
@@ -4,12 +4,15 @@
 #include <target/types.h>
 #include <asm/muldiv.h>
 
+/* 16-bit calculations */
 #ifndef ARCH_HAVE_DIV16U
 uint16_t div16u(uint16_t x, uint16_t y);
 #endif
 #ifndef ARCH_HAVE_MOD16U
 uint16_t mod16u(uint16_t x, uint16_t y);
 #endif
+
+/* 32-bit calculations */
 #ifndef ARCH_HAVE_DIV32U
 uint32_t div32u(uint32_t x, uint32_t y);
 #endif
@@ -20,9 +23,14 @@ uint32_t mod32u(uint32_t x, uint32_t y);
 uint32_t mul16u(uint16_t x, uint16_t y);
 #endif
 
-#ifdef ARCH_HAVE_UINT64_T
+/* 64-bit calculations */
+#ifndef ARCH_HAVE_MUL32U
 uint64_t mul32u(uint32_t x, uint32_t y);
+#endif
+#ifndef ARCH_HAVE_MOD64U
 uint64_t mod64u(uint64_t x, uint64_t y);
+#endif
+#ifndef ARCH_HAVE_DIV64U
 uint64_t div64u(uint64_t x, uint64_t y);
 #endif
 
