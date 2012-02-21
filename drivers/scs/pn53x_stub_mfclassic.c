2a14d5628ef9b0582f406d6eb89337e559b5559e
Lv Zheng <zetalog@gmail.com>
2012-02-21 12:16:22 +0800

    update mfclassic stub framework codes.

--- drivers/scs/pn53x_stub_mfclassic.c
+++ drivers/scs/pn53x_stub_mfclassic.c
@@ -1,4 +1,6 @@
 #include <driver/pn53x.h>
+#include <target/mtd.h>
+#include <target/arch.h>
 
 #define MFCLASSIC_CMD_AUTH_A	0x60
 #define MFCLASSIC_CMD_AUTH_B	0x61
@@ -10,6 +12,7 @@
 #define MFCLASSIC_CMD_STORE	0xC2
 
 #define MFCLASSIC_RESP_INIT	0xFF
+#define MFCLASSIC_RESP_NAK	0x00
 #define MFCLASSIC_RESP_ACK	0x0A
 
 struct mfclassic_auth {
@@ -37,6 +40,8 @@ uint8_t mfclassic_cmd_len;
 uint8_t mfclassic_code;
 uint8_t mfclassic_cmd[MFCLASSIC_MAX_BUF];
 uint8_t mfclassic_resp[MFCLASSIC_MAX_BUF];
+
+extern mtd_t board_mtd;
 
 scs_err_t pn53x_stub_mfclassic_get_info(uint8_t tg,
 					union nfc_target_info *nti)
@@ -80,6 +85,51 @@ uint8_t pn53x_stub_mfclassic_read_byte(uint8_t tg, scs_off_t index)
 	return 0;
 }
 
+static void pn53x_stub_mfclassic_error(uint8_t err)
+{
+	mfclassic_code = (err & 0x0F);
+}
+
+void pn53x_stub_mfclassic_auth_a(void)
+{
+	pn53x_stub_mfclassic_error(MFCLASSIC_RESP_NAK);
+}
+
+void pn53x_stub_mfclassic_auth_b(void)
+{
+	pn53x_stub_mfclassic_error(MFCLASSIC_RESP_NAK);
+}
+
+void pn53x_stub_mfclassic_read(void)
+{
+	pn53x_stub_mfclassic_error(MFCLASSIC_RESP_NAK);
+}
+
+void pn53x_stub_mfclassic_write(void)
+{
+	pn53x_stub_mfclassic_error(MFCLASSIC_RESP_NAK);
+}
+
+void pn53x_stub_mfclassic_transfer(void)
+{
+	pn53x_stub_mfclassic_error(MFCLASSIC_RESP_NAK);
+}
+
+void pn53x_stub_mfclassic_decrement(void)
+{
+	pn53x_stub_mfclassic_error(MFCLASSIC_RESP_NAK);
+}
+
+void pn53x_stub_mfclassic_increment(void)
+{
+	pn53x_stub_mfclassic_error(MFCLASSIC_RESP_NAK);
+}
+
+void pn53x_stub_mfclassic_store(void)
+{
+	pn53x_stub_mfclassic_error(MFCLASSIC_RESP_NAK);
+}
+
 void pn53x_stub_mfclassic_write_ready(uint8_t tg, scs_size_t count)
 {
 	if (tg != 0)
@@ -91,20 +141,31 @@ void pn53x_stub_mfclassic_write_ready(uint8_t tg, scs_size_t count)
 
 	switch (mfclassic_cmd[0]) {
 	case MFCLASSIC_CMD_AUTH_A:
+		pn53x_stub_mfclassic_auth_a();
 		break;
 	case MFCLASSIC_CMD_AUTH_B:
+		pn53x_stub_mfclassic_auth_b();
 		break;
 	case MFCLASSIC_CMD_READ:
+		pn53x_stub_mfclassic_read();
 		break;
 	case MFCLASSIC_CMD_WRITE:
+		pn53x_stub_mfclassic_write();
 		break;
 	case MFCLASSIC_CMD_TRANSFER:
+		pn53x_stub_mfclassic_transfer();
 		break;
 	case MFCLASSIC_CMD_DECREMENT:
+		pn53x_stub_mfclassic_decrement();
 		break;
 	case MFCLASSIC_CMD_INCREMENT:
+		pn53x_stub_mfclassic_increment();
 		break;
 	case MFCLASSIC_CMD_STORE:
+		pn53x_stub_mfclassic_store();
+		break;
+	default:
+		pn53x_stub_mfclassic_error(MFCLASSIC_RESP_NAK);
 		break;
 	}
 }
@@ -117,7 +178,7 @@ void pn53x_stub_mfclassic_write_byte(uint8_t tg, scs_off_t index, uint8_t value)
 }
 
 pn53x_stub_driver_t pn53x_stub_mfclassic = {
-	0/* NFC_MODULATION(NFC_TYPE_ISO14443A, NFC_BAUD_106) */,
+	NFC_MODULATION(NFC_TYPE_ISO14443A, NFC_BAUD_106),
 	pn53x_stub_mfclassic_get_info,
 	pn53x_stub_mfclassic_read_ready,
 	pn53x_stub_mfclassic_read_count,
