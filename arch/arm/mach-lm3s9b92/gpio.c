75af2affb1ac1146ff60802f38abb07c9d825cf5
Lv Zheng <zetalog@gmail.com>
2012-04-10 17:56:11 +0800

    fix lm3s9b92 default in/out drive bugs.

--- arch/arm/mach-lm3s9b92/gpio.c
+++ arch/arm/mach-lm3s9b92/gpio.c
@@ -99,7 +99,11 @@ void gpio_hw_config_pad(uint8_t port, uint8_t pin,
 
 	reg = __gpio_hw_port_reg(port, GPIODR2R);
 	__raw_clearl_atomic(pin, reg);
-	if (drv > 0 && drv <= 2) {
+	if (drv <= 2) {
+		/* XXX: GPIO Bi-directional Drive
+		 * If a PIN can be both read and writen, it should be a
+		 * digital IO, 2mA drive should be enough for such PINs.
+		 */
 		__raw_setl_atomic(pin, reg);
 	}
 	reg = __gpio_hw_port_reg(port, GPIODR4R);
