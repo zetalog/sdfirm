0a60f98059ad057a21b4527115e72adfd24c98e9
Lv Zheng <zetalog@gmail.com>
2012-04-16 05:38:45 +0800

    rename state.c to bh.c.

---
+++ kernel/bh.c
@@ -0,0 +1,107 @@
+#include <target/state.h>
+#include <target/irq.h>
+
+DECLARE_BITMAP(bh_awakes, NR_BHS);
+struct bh_entry bh_entries[NR_BHS];
+bh_t bh_nr_regs = 0;
+DECLARE_BITMAP(poll_regs, NR_BHS);
+
+/* big background lock */
+#ifdef SYS_REALTIME
+/* XXX: Interrupted Timer Handled In Time
+ *
+ * Whenever we talk about the realtime, what we mean include followings:
+ * 1. interrupt timer (itimer):
+ *    timeout events can be raised in time by the system
+ * 2. real time clock (rtc):
+ *    both of the communication entities can refer to a 3rd party
+ *    authoritative "real" time axis
+ *
+ * Referring to a real time axis is critical for system throuputs.  Since
+ * throuput varies inversely with time, a stretched time axis will always
+ * lead to the lower throuputs.  To ensure real time axis, system codes
+ * need to measure a 3rd party clock authority, then correct the time
+ * error in their algorithm based on the measurements.  Which means it is
+ * ensured by the system codes' quality, not by the system architecture.
+ *
+ * Since we are talking about realtime in the system architecture, it
+ * implies the in time timer issue.  No global IRQ masking means timeout
+ * handler in the interrupt context can be handled in time!
+ */
+#define lock_bh()
+#define unlock_bh()
+#else
+#define lock_bh()			irq_local_disable()
+#define unlock_bh()			irq_local_enable()
+#endif
+
+void poll_run(bh_t bh)
+{
+	if (test_bit(bh, poll_regs))
+		bh_run(bh, BH_POLLIRQ);
+}
+
+void poll_register(bh_t bh)
+{
+	set_bit(bh, poll_regs);
+}
+
+void __bh_run(bh_t bh, uint8_t event)
+{
+	bh_entries[bh].handler(event);
+}
+
+void bh_run(bh_t bh, uint8_t event)
+{
+	lock_bh();
+	idle_debug(IDLE_DEBUG_SID, bh);
+	__bh_run(bh, event);
+	unlock_bh();
+}
+
+void bh_suspend(bh_t bh)
+{	
+	clear_bit((bh), bh_awakes);
+}
+
+void bh_resume(bh_t bh)
+{
+	set_bit(bh, bh_awakes);
+}
+
+boolean bh_resumed_any(void)
+{
+	return NR_BHS == find_next_set_bit(bh_awakes, NR_BHS, 0);
+}
+
+bh_t bh_register_handler(bh_cb handler)
+{
+	bh_t bh = bh_nr_regs;
+
+	BUG_ON(bh == NR_BHS);
+	bh_entries[bh].handler = handler;
+	bh_nr_regs++;
+	return bh;
+}
+
+void bh_run_all(void)
+{
+	bh_t bh;
+
+	for (bh = 0; bh < bh_nr_regs; bh++) {
+		if (test_bit(bh, bh_awakes)) {
+			bh_suspend(bh);
+			bh_run(bh, BH_WAKEUP);
+		}
+		poll_run(bh);
+	}
+}
+
+void bh_panic(void)
+{
+	idle_debug(IDLE_DEBUG_SID, DBG_SRC_IDLE);
+}
+
+void bh_init(void)
+{
+}
