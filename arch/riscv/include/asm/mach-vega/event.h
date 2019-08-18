f48dc0309fe4376400eb5e48041cb2ab5ba390c1
Lv Zheng <zhenglv@hotmail.com>
2019-08-18 12:13:38 +0800

    Add RV32M1(VEGA) IRQ controller definition
    This patch adds event unit definitions for mach-vega.
    However, the functionality of IRQC is not implemented yet.
    
    Signed-off-by: Lv Zheng <zhenglv@hotmail.com>

---
+++ arch/riscv/include/asm/mach-vega/event.h
@@ -0,0 +1,123 @@
+/*
+ * ZETALOG's Personal COPYRIGHT
+ *
+ * Copyright (c) 2019
+ *    ZETALOG - "Lv ZHENG".  All rights reserved.
+ *    Author: Lv "Zetalog" Zheng
+ *    Internet: zhenglv@hotmail.com
+ *
+ * This COPYRIGHT used to protect Personal Intelligence Rights.
+ * Redistribution and use in source and binary forms with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *    This product includes software developed by the Lv "Zetalog" ZHENG.
+ * 3. Neither the name of this software nor the names of its developers may
+ *    be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ * 4. Permission of redistribution and/or reuse of souce code partially only
+ *    granted to the developer(s) in the companies ZETALOG worked.
+ * 5. Any modification of this software should be published to ZETALOG unless
+ *    the above copyright notice is no longer declaimed.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE ZETALOG AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE ZETALOG OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * @(#)event.h: RV32M1 (VEGA) event unit (IRQC) driver
+ * $Id: event.h,v 1.1 2019-08-18 12:09:00 zhenglv Exp $
+ */
+
+#ifndef __EVENT_VEGA_H_INCLUDE__
+#define __EVENT_VEGA_H_INCLUDE__
+
+/* RI5CY and ZERO-RISCY core both have an event unit to support vectorized
+ * interrupts of up to 32 lines and event triggering of up to 32 input
+ * lines. The interrupt and event lines are separately masked and buffered.
+ */
+#ifdef CONFIG_VEGA_RI5CY
+#define EVENT_BASE		UL(0xE0041000)
+#endif
+#ifdef CONFIG_VEGA_0RISCY
+#define EVENT_BASE		UL(0x4101F000)
+#endif
+#define EVENT_REG(off)		(EVENT_BASE + (off))
+#define EVENT_1BIT_REG(off, n)	REG_1BIT_REG(EVENT_BASE+(off), n)
+#define EVENT_4BIT_REG(off, n)	REG_4BIT_REG(EVENT_BASE+(off), n)
+
+#define EVENT_INTPTEN(n)	EVENT_1BIT_REG(0x00, n)
+#define EVENT_INTPTPEND		EVENT_REG(0x04)
+#define EVENT_INTPTPENDSET	EVENT_REG(0x08)
+#define EVENT_INTPTPENDCLEAR	EVENT_REG(0x0C)
+#define EVENT_INTPTSECURE	EVENT_REG(0x10)
+#define EVENT_INTPTPRI(n)	EVENT_4BIT_REG(0x14, n)
+#define EVENT_INTPRIBASE	EVENT_REG(0x24)
+#define EVENT_INTPTENACTIVE	EVENT_REG(0x28)
+#define EVENT_INTACTPRI(n)	EVENT_4BIT_REG(0x2C, n)
+#define EVENT_EVENTEN		EVENT_REG(0x40)
+#define EVENT_EVENTPEND		EVENT_REG(0x44)
+#define EVENT_EVTPENDSET	EVENT_REG(0x48)
+#define EVENT_EVTPENDCLEAR	EVENT_REG(0x4C)
+#define EVENT_SLPCTRL		EVENT_REG(0x80)
+#define EVENT_SLPSTATUS		EVENT_REG(0x84)
+
+/* EVENT_IRQ: applies to the following 1BIT registers
+ * 3.4.2.1 Interrupt Enable Register (EVENTx_INTPTEN)
+ * 3.4.2.2 Interrupt Pending Register (EVENTx_INTPTPEND)
+ * 3.4.2.3 Set Interrupt Pending Register (EVENTx_INTPTPENDSET)
+ * 3.4.2.4 Clear Interrupt Pending Register (EVENTx_INTPTPENDCLEAR)
+ * 3.4.2.5 Interrupt Secure Register (EVENTx_INTPTSECURE)
+ * 3.4.2.11 Interrupt Active Register (EVENTx_INTPTENACTIVE)
+ */
+#define EVENT_IRQ(n)			_BV(REG_1BIT_OFFSET(n))
+
+/* EVENT_EVT: applies to the following 1BIT registers
+ * 3.4.2.16 Event Enable Register (EVENTx_EVENTEN)
+ * 3.4.2.17 Event Pending Register (EVENTx_EVENTPEND)
+ * 3.4.2.18 Set Event Pending Register (EVENTx_EVTPENDSET)
+ * 3.4.2.19 Clear Event Pending Register (EVENTx_EVTPENDCLEAR)
+ */
+#define EVENT_EVT(n)			_BV(REG_1BIT_OFFSET(n))
+
+/* EVENT_PRI: applies to the following 4BIT registers
+ * 3.4.2.6-9 Interrupt Priority 0-3 Register (EVENTx_INTPTPRI0-3)
+ * 3.4.2.12-15 Interrupt Active Priority 0-3 Register (EVENTx_INTACTPRI0-3)
+ */
+#define EVENT_PRI_MAX			7
+#define EVENT_PRI_MASK(n)		REG_4BIT_MASK
+#define EVENT_PRI_OFFSET(n)		REG_4BIT_OFFSET(n)
+#define EVENT_PRI(n, value)		\
+	_SET_FVn(n, EVENT_PRI, (value) & EVENT_PRI_MAX)
+
+/* 3.4.2.10 Interrupt Priority Base (EVENTx_INTPRIBASE) */
+#define EVENT_IPBASE_OFFSET		0
+#define EVENT_IPBASE_MASK		REG_4BIT_MASK
+#define EVENT_IPBASE(value)		_SET_FV(EVENT_IPBASE, value)
+
+/* 3.4.2.20 Sleep Control Register (EVENTx_SLPCTRL) */
+#define SLPCTRL_SLPCTRL_OFFSET		0
+#define SLPCTRL_SLPCTRL_MASK		REG_2BIT_MASK
+#define SLPCTRL_SLPCTRL(value)		_SET_FV(SLPCTRL_SLPCTRL, value)
+#define SLPCTRL_SYSRSTREQST		_BV(31)
+
+/* 3.4.2.21 Sleep Status Register (EVENTx_SLPSTATUS) */
+#define SLPSTATUS_SLPSTAT_OFFSET	0
+#define SLPSTATUS_SLPSTAT_MASK		REG_2BIT_MASK
+#define SLPSTATUS_SLPSTAT(value)	_GET_FV(SLPSTATUS_SLPSTAT, value)
+
+/* The sleep ctrl/stat value */
+#define EVENT_SLP_SLEEP_ENABLE		0x1
+#define EVENT_SLP_DEEP_SLEEP_ENABLE	0x2
+
+#endif /* __EVENT_VEGA_H_INCLUDE__ */
--- include/target/types.h
+++ include/target/types.h
@@ -13,64 +13,76 @@
 #include <stdbool.h>
 
 #ifdef ARCH_HAS_BOOL
-typedef bool			boolean;
+typedef bool				boolean;
 #else
-typedef uint8_t			boolean;
-typedef boolean			bool;
-#define false			(boolean)0
-#define true			(!false)
+typedef uint8_t				boolean;
+typedef boolean				bool;
+#define false				(boolean)0
+#define true				(!false)
 #endif
-typedef uint16_t		wchar_t;
+typedef uint16_t			wchar_t;
 #ifndef ARCH_HAVE_CADDR_T
-typedef unsigned long		caddr_t;
+typedef unsigned long			caddr_t;
 #endif
 #ifndef ARCH_HAVE_UINTPTR_T
-typedef unsigned long		uintptr_t;
+typedef unsigned long			uintptr_t;
 #endif
 #ifndef ARCH_HAVE_SIZE_T
-typedef unsigned long		size_t;
+typedef unsigned long			size_t;
 #endif
 #ifndef ARCH_HAVE_LOFF_T
-typedef unsigned long		loff_t;
+typedef unsigned long			loff_t;
 #endif
 
 #ifndef NULL
-#define	NULL			((void *)0L)
+#define	NULL				((void *)0L)
 #endif /* NULL */
 
 #undef offsetof
-#define offsetof(TYPE, MEMBER)	((size_t)&((TYPE *)0)->MEMBER)
+#define offsetof(TYPE, MEMBER)		((size_t)&((TYPE *)0)->MEMBER)
 #endif /* __ASSEMBLY__ */
 
-#define _BV_UL(bit)		(UL(1) << (bit))
-#define _BV_ULL(bit)		(ULL(1) << (bit))
-#define _BV(bit)		_BV_UL(bit)
-#define _SET_FV(name, value)	\
+#define _BV_UL(bit)			(UL(1) << (bit))
+#define _BV_ULL(bit)			(ULL(1) << (bit))
+#define _BV(bit)			_BV_UL(bit)
+#define _SET_FV(name, value)		\
 	(((value) & (name##_MASK)) << (name##_OFFSET))
-#define _GET_FV(name, value)	\
+#define _GET_FV(name, value)		\
 	(((value) >> (name##_OFFSET)) & (name##_MASK))
 /* Default to _SET_FV() as by default _FV() is used to generate field
  * values to be written to the registers.
  */
-#define _FV(name, value)	_SET_FV(name, value)
+#define _FV(name, value)		_SET_FV(name, value)
 
-#define REG_1BIT_MASK		0x001
-#define REG_2BIT_MASK		0x003
-#define REG_3BIT_MASK		0x007
-#define REG_4BIT_MASK		0x00F
-#define REG_5BIT_MASK		0x01F
-#define REG_6BIT_MASK		0x03F
-#define REG_7BIT_MASK		0x07F
-#define REG_8BIT_MASK		0x0FF
-#define REG_9BIT_MASK		0x1FF
-#define REG_10BIT_MASK		0x3FF
-#define REG_11BIT_MASK		0x7FF
-#define REG_12BIT_MASK		0xFFF
-#define REG_13BIT_MASK		0x1FFF
-#define REG_16BIT_MASK		0xFFFF
-#define REG_17BIT_MASK		0x1FFFF
-#define REG_20BIT_MASK		0xFFFFF
-#define REG_24BIT_MASK		0xFFFFFF
+#define REG_1BIT_MASK			0x001
+#define REG_2BIT_MASK			0x003
+#define REG_3BIT_MASK			0x007
+#define REG_4BIT_MASK			0x00F
+#define REG_5BIT_MASK			0x01F
+#define REG_6BIT_MASK			0x03F
+#define REG_7BIT_MASK			0x07F
+#define REG_8BIT_MASK			0x0FF
+#define REG_9BIT_MASK			0x1FF
+#define REG_10BIT_MASK			0x3FF
+#define REG_11BIT_MASK			0x7FF
+#define REG_12BIT_MASK			0xFFF
+#define REG_13BIT_MASK			0x1FFF
+#define REG_16BIT_MASK			0xFFFF
+#define REG_17BIT_MASK			0x1FFFF
+#define REG_20BIT_MASK			0xFFFFF
+#define REG_24BIT_MASK			0xFFFFFF
+#define REG_1BIT_ADDR(base, n)		((base)+(((n) & (~31)) >> 3))
+#define REG_2BIT_ADDR(base, n)		((base)+(((n) & (~15)) >> 2))
+#define REG_4BIT_ADDR(base, n)		((base)+(((n) & (~7 )) >> 1))
+#define REG_8BIT_ADDR(base, n)		((base)+(((n) & (~3 )) >> 0))
+#define REG_1BIT_OFFSET(n)		(((n) & 31) << 0)
+#define REG_2BIT_OFFSET(n)		(((n) & 15) << 1)
+#define REG_4BIT_OFFSET(n)		(((n) &  7) << 2)
+#define REG_8BIT_OFFSET(n)		(((n) &  3) << 3)
+#define _GET_FVn(n, name, value)	\
+	(((value) >> (name##_OFFSET(n))) & (name##_MASK))
+#define _SET_FVn(n, name, value)	\
+	(((value) & (name##_MASK)) << (name##_OFFSET(n)))
 
 #ifndef __ASSEMBLY__
 __TEXT_TYPE__(char, text_char_t);
