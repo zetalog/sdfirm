696a9a37aee2691aeec34147ef72a9f163122fd3
Lv Zheng <zhenglv@hotmail.com>
2019-08-19 14:11:06 +0800

    Update VEGA PCC definitions
    This patch updates VEGA PCC definitions.
    
    Signed-off-by: Lv Zheng <zhenglv@hotmail.com>

--- arch/riscv/include/asm/mach-vega/pcc.h
+++ arch/riscv/include/asm/mach-vega/pcc.h
@@ -36,145 +36,144 @@
  * SUCH DAMAGE.
  *
  * @(#)pcc.h: RV32M1 (VEGA) PCC (peripheral clock control) driver
- * $Id: pcc.h,v 1.1 2019-08-17 16:30:00 zhenglv Exp $
+ * $Id: pcc.h,v 1.1 2019-08-19 13:49:00 zhenglv Exp $
  */
 
 #ifndef __PCC_VEGA_H_INCLUDE__
 #define __PCC_VEGA_H_INCLUDE__
 
-#define PCC0_BASE		UL(0x4002B000)
-#define PCC1_BASE		UL(0x41027000)
-#define PCC_REG(n, offset)	(PCC##n##_BASE + (offset))
+#define PCC0_BASE			UL(0x4002B000)
+#define PCC1_BASE			UL(0x41027000)
+#define PCC_REG(n, offset)		(PCC##n##_BASE + (offset))
 
 /* PCC 0 */
-#define PCC_Mscm		PCC_REG(0, 0x004)
-#define PCC_Syspm		PCC_REG(0, 0x00C)
-#define PCC_Max0		PCC_REG(0, 0x010)
-#define PCC_Edma0		PCC_REG(0, 0x020)
-#define PCC_Flexbus		PCC_REG(0, 0x030)
-#define PCC_Xrdc0Mgr		PCC_REG(0, 0x050)
-#define PCC_Xrdc0Pac		PCC_REG(0, 0x058)
-#define PCC_Xrdc0Mrc		PCC_REG(0, 0x05C)
-#define PCC_Sema420		PCC_REG(0, 0x06C)
-#define PCC_Dmamux0		PCC_REG(0, 0x084)
-#define PCC_Ewm0		PCC_REG(0, 0x088)
-#define PCC_MuA			PCC_REG(0, 0x094)
-#define PCC_Crc0		PCC_REG(0, 0x0BC)
-#define PCC_Lpit0		PCC_REG(0, 0x0C0)
-#define PCC_Tpm0		PCC_REG(0, 0x0D4)
-#define PCC_Tpm1		PCC_REG(0, 0x0D8)
-#define PCC_Tpm2		PCC_REG(0, 0x0DC)
-#define PCC_Emvsim0		PCC_REG(0, 0x0E0)
-#define PCC_Flexio0		PCC_REG(0, 0x0E4)
-#define PCC_Lpi2c0		PCC_REG(0, 0x0E8)
-#define PCC_Lpi2c1		PCC_REG(0, 0x0EC)
-#define PCC_Lpi2c2		PCC_REG(0, 0x0F0)
-#define PCC_Sai0		PCC_REG(0, 0x0F4)
-#define PCC_Sdhc0		PCC_REG(0, 0x0F8)
-#define PCC_Lpspi0		PCC_REG(0, 0x0FC)
-#define PCC_Lpspi1		PCC_REG(0, 0x100)
-#define PCC_Lpspi2		PCC_REG(0, 0x104)
-#define PCC_Lpuart0		PCC_REG(0, 0x108)
-#define PCC_Lpuart1		PCC_REG(0, 0x10C)
-#define PCC_Lpuart2		PCC_REG(0, 0x110)
-#define PCC_Usb0		PCC_REG(0, 0x114)
-#define PCC_PortA		PCC_REG(0, 0x118)
-#define PCC_PortB		PCC_REG(0, 0x11C)
-#define PCC_PortC		PCC_REG(0, 0x120)
-#define PCC_PortD		PCC_REG(0, 0x124)
-#define PCC_Lpadc0		PCC_REG(0, 0x128)
-#define PCC_Dac0		PCC_REG(0, 0x130)
-#define PCC_Vref		PCC_REG(0, 0x134)
-#define PCC_Atx			PCC_REG(0, 0x138)
-#define PCC_Intmux0		PCC_REG(0, 0x13C)
-#define PCC_Trace		PCC_REG(0, 0x200)
+#define PCC_MSCM			PCC_REG(0, 0x004)
+/* #define PCC_SYSPM			PCC_REG(0, 0x00C) */
+#define PCC_AXBS0			PCC_REG(0, 0x010)
+#define PCC_DMA0			PCC_REG(0, 0x020)
+#define PCC_FLEXBUS			PCC_REG(0, 0x030)
+#define PCC_XRDC_MGR			PCC_REG(0, 0x050)
+#define PCC0_XRDC_PAC			PCC_REG(0, 0x058)
+#define PCC0_XRDC_MRC			PCC_REG(0, 0x05C)
+#define PCC_SEMA42_0			PCC_REG(0, 0x06C)
+#define PCC_DMAMUX0			PCC_REG(0, 0x084)
+#define PCC_EWM				PCC_REG(0, 0x088)
+#define PCC_MUA				PCC_REG(0, 0x094)
+#define PCC_CRC0			PCC_REG(0, 0x0BC)
+#define PCC_LPIT0			PCC_REG(0, 0x0C0)
+#define PCC_TPM0			PCC_REG(0, 0x0D4)
+#define PCC_TPM1			PCC_REG(0, 0x0D8)
+#define PCC_TPM2			PCC_REG(0, 0x0DC)
+#define PCC_EMVSIM0			PCC_REG(0, 0x0E0)
+#define PCC_FLEXIO0			PCC_REG(0, 0x0E4)
+#define PCC_LPI2C0			PCC_REG(0, 0x0E8)
+#define PCC_LPI2C1			PCC_REG(0, 0x0EC)
+#define PCC_LPI2C2			PCC_REG(0, 0x0F0)
+#define PCC_I2S0			PCC_REG(0, 0x0F4)
+#define PCC_USDHC0			PCC_REG(0, 0x0F8)
+#define PCC_LPSPI0			PCC_REG(0, 0x0FC)
+#define PCC_LPSPI1			PCC_REG(0, 0x100)
+#define PCC_LPSPI2			PCC_REG(0, 0x104)
+#define PCC_LPUART0			PCC_REG(0, 0x108)
+#define PCC_LPUART1			PCC_REG(0, 0x10C)
+#define PCC_LPUART2			PCC_REG(0, 0x110)
+#define PCC_USB0			PCC_REG(0, 0x114)
+#define PCC_PORTA			PCC_REG(0, 0x118)
+#define PCC_PORTB			PCC_REG(0, 0x11C)
+#define PCC_PORTC			PCC_REG(0, 0x120)
+#define PCC_PORTD			PCC_REG(0, 0x124)
+#define PCC_LPADC0			PCC_REG(0, 0x128)
+#define PCC_LPDAC0			PCC_REG(0, 0x130)
+#define PCC_VREF			PCC_REG(0, 0x134)
+/* #define PCC_ATX			PCC_REG(0, 0x138) */
+/* #define PCC_INTMUX0			PCC_REG(0, 0x13C) */
+#define PCC_TRACE			PCC_REG(0, 0x200)
 /* PCC 1 */
-#define PCC_Edma1		PCC_REG(1, 0x020)
-#define PCC_Rgpio1		PCC_REG(1, 0x03C)
-#define PCC_Xrdc0PacB		PCC_REG(1, 0x058)
-#define PCC_Xrdc0MrcB		PCC_REG(1, 0x05C)
-#define PCC_Sema421		PCC_REG(1, 0x06C)
-#define PCC_Dmamux1		PCC_REG(1, 0x084)
-#define PCC_Intmux1		PCC_REG(1, 0x088)
-#define PCC_Intmux0		PCC_REG(1, 0x088)
-#define PCC_MuB			PCC_REG(1, 0x090)
-#define PCC_Cau3		PCC_REG(1, 0x0A0)
-#define PCC_Trng		PCC_REG(1, 0x0A4)
-#define PCC_Lpit1		PCC_REG(1, 0x0A8)
-#define PCC_Tpm3		PCC_REG(1, 0x0B4)
-#define PCC_Lpi2c3		PCC_REG(1, 0x0B8)
-#define PCC_Lpspi3		PCC_REG(1, 0x0D4)
-#define PCC_Lpuart3		PCC_REG(1, 0x0D8)
-#define PCC_PortE		PCC_REG(1, 0x0DC)
-#define PCC_Ext0		PCC_REG(1, 0x200)
-#define PCC_Ext1		PCC_REG(1, 0x204)
+#define PCC_DMA1			PCC_REG(1, 0x020)
+#define PCC_GPIOE			PCC_REG(1, 0x03C)
+#define PCC1_XRDC_PAC			PCC_REG(1, 0x058)
+#define PCC1_XRDC_MRC			PCC_REG(1, 0x05C)
+#define PCC_SEMA42_1			PCC_REG(1, 0x06C)
+#define PCC_DMAMUX1			PCC_REG(1, 0x084)
+#define PCC_INTMUX1			PCC_REG(1, 0x088)
+#define PCC_MUB				PCC_REG(1, 0x090)
+#define PCC_CAU3			PCC_REG(1, 0x0A0)
+#define PCC_TRNG			PCC_REG(1, 0x0A4)
+#define PCC_LPIT1			PCC_REG(1, 0x0A8)
+#define PCC_TPM3			PCC_REG(1, 0x0B4)
+#define PCC_LPI2C3			PCC_REG(1, 0x0B8)
+#define PCC_LPSPI3			PCC_REG(1, 0x0D4)
+#define PCC_LPUART3			PCC_REG(1, 0x0D8)
+#define PCC_PORTE			PCC_REG(1, 0x0DC)
+#define PCC_MTB				PCC_REG(1, 0x200)
+#define PCC_EXT_CLK			PCC_REG(1, 0x204)
 
-#define PCC_CLKCFG_PCD_MASK	REG_3BIT_MASK
-#define PCC_CLKCFG_PCD_SHIFT	0
-/* PCD - Peripheral Clock Divider Select
- *  0b000..Divide by 1.
- *  0b001..Divide by 2.
- *  0b010..Divide by 3.
- *  0b011..Divide by 4.
- *  0b100..Divide by 5.
- *  0b101..Divide by 6.
- *  0b110..Divide by 7.
- *  0b111..Divide by 8.
+/* The following peripherals support PCD/FRAC fields:
+ * 30.5.1.36 PCC LPADC0 Register (PCC_LPADC0)
+ * 30.5.1.39 PCC TRACE Register (PCC_TRACE)
  */
-#define PCC_CLKCFG_PCD(x)	\
-	(((uint32_t)(((uint32_t)(x)) << PCC_CLKCFG_PCD_SHIFT)) & PCC_CLKCFG_PCD_MASK)
-#define PCC_CLKCFG_FRAC_MASK	(0x8U)
-#define PCC_CLKCFG_FRAC_SHIFT	3
-/* FRAC - Peripheral Clock Divider Fraction
- *  0b0..Fractional value is 0.
- *  0b1..Fractional value is 1.
+#define PCC_PCD_OFFSET			0
+#define PCC_PCD_MASK			REG_3BIT_MASK
+#define PCC_PCD(value)			_SET_FV(PCC_PCD, value)
+/* Divide by 1, 2, 3, 4, 5, 6, 7, 8 */
+#define PCC_PCD_DIV(div)		PCD_PCD((div) - 1)
+#define PCD_FRAC			_BV(3)
+
+/* PCS: Peripheral Clock Select
+ * The following peripherals support PCS fields:
+ * 30.5.1.14 PCC LPIT0 Register (PCC_LPIT0)
+ * 30.5.1.15 PCC TPM0 Register (PCC_TPM0)
+ * 30.5.1.16 PCC TPM1 Register (PCC_TPM1)
+ * 30.5.1.17 PCC TPM2 Register (PCC_TPM2)
+ * 30.5.1.18 PCC EMVSIM0 Register (PCC_EMVSIM0)
+ * 30.5.1.19 PCC FLEXIO0 Register (PCC_FLEXIO0)
+ * 30.5.1.20 PCC LPI2C0 Register (PCC_LPI2C0)
+ * 30.5.1.21 PCC LPI2C1 Register (PCC_LPI2C1)
+ * 30.5.1.22 PCC LPI2C2 Register (PCC_LPI2C2)
+ * 30.5.1.23 PCC I2S0 Register (PCC_I2S0)
+ * 30.5.1.24 PCC USDHC0 Register (PCC_USDHC0)
+ * 30.5.1.25 PCC LPSPI0 Register (PCC_LPSPI0)
+ * 30.5.1.26 PCC LPSPI1 Register (PCC_LPSPI1)
+ * 30.5.1.27 PCC LPSPI2 Register (PCC_LPSPI2)
+ * 30.5.1.28 PCC LPUART0 Register (PCC_LPUART0)
+ * 30.5.1.29 PCC LPUART1 Register (PCC_LPUART1)
+ * 30.5.1.30 PCC LPUART2 Register (PCC_LPUART2)
+ * 30.5.1.31 PCC USB0 Register (PCC_USB0)
+ * 30.5.1.36 PCC LPADC0 Register (PCC_LPADC0)
+ * 30.5.1.39 PCC TRACE Register (PCC_TRACE)
+ * 30.5.2.12 PCC LPIT1 Register (PCC_LPIT1)
+ * 30.5.2.13 PCC TPM3 Register (PCC_TPM3)
+ * 30.5.2.14 PCC LPI2C3 Register (PCC_LPI2C3)
+ * 30.5.2.15 PCC LPSPI3 Register (PCC_LPSPI3)
+ * 30.5.2.16 PCC LPUART3 Register (PCC_LPUART3)
  */
-#define PCC_CLKCFG_FRAC(x)	\
-	(((uint32_t)(((uint32_t)(x)) << PCC_CLKCFG_FRAC_SHIFT)) & PCC_CLKCFG_FRAC_MASK)
-
-#define PCC_CLKCFG_PCS_MASK	(0x7000000U)
-#define PCC_CLKCFG_PCS_SHIFT	24
-/* PCS - Peripheral Clock Source Select
- *  0b000..Clock is off. An external clock can be enabled for this peripheral.
- *  0b001..Clock option 1
- *  0b010..Clock option 2
- *  0b011..Clock option 3
- *  0b100..Clock option 4
- *  0b101..Clock option 5
- *  0b110..Clock option 6
- *  0b111..Clock option 7
+#define PCC_PCS_OFFSET			24
+#define PCC_PCS_MASK			REG_3BIT_MASK
+#define PCC_PCS(value)			_SET_FV(PCC_PCS, value)
+/* 0b000:
+ *  Clock is off. Or an external clock can be enabled for this peripheral.
+ *  Only LPADC and I2S support external clock:
+ *    The external clock to I2S is I2S0_MCLK from PTB5 or PTE19.
+ *    The external clock to LPADC is the output of LPADCIRC.
  */
-#define PCC_CLKCFG_PCS(x)	\
-	(((uint32_t)(((uint32_t)(x)) << PCC_CLKCFG_PCS_SHIFT)) & PCC_CLKCFG_PCS_MASK)
+#define PCC_PCS_OFF			0
+#define PCC_PCS_EXT			0
+#define PCC_PCS_SYS_OSC			1
+#define PCC_PCS_SIRC			2
+#define PCC_PCS_FIRC			3
+#define PCC_PCS_LPFLL			6
 
-#define PCC_CLKCFG_INUSE_MASK	UL(0x20000000)
-#define PCC_CLKCFG_INUSE_SHIFT	29
-/* INUSE - In use flag
- *  0b0..Peripheral is not being used.
- *  0b1..Peripheral is being used. Software cannot modify the existing clocking configuration.
- */
-#define PCC_CLKCFG_INUSE(x)	\
-	(((uint32_t)(((uint32_t)(x)) << PCC_CLKCFG_INUSE_SHIFT)) & PCC_CLKCFG_INUSE_MASK)
+#define PCC_INUSE			_BV(29)
+#define PCC_CGC				_BV(30)
+#define PCC_PR				_BV(31)
 
-#define PCC_CLKCFG_CGC_MASK	(0x40000000U)
-#define PCC_CLKCFG_CGC_SHIFT	30
-/* CGC - Clock Gate Control
- *  0b0..Clock disabled
- *  0b1..Clock enabled. The current clock selection and divider options are locked.
- **/
-#define PCC_CLKCFG_CGC(x)	\
-	(((uint32_t)(((uint32_t)(x)) << PCC_CLKCFG_CGC_SHIFT)) & PCC_CLKCFG_CGC_MASK)
-
-#define PCC_CLKCFG_PR_MASK	(0x80000000U)
-#define PCC_CLKCFG_PR_SHIFT	31
-/* PR - Present
- *  0b0..Peripheral is not present.
- *  0b1..Peripheral is present.
- */
-#define PCC_CLKCFG_PR(x)	\
-	(((uint32_t)(((uint32_t)(x)) << PCC_CLKCFG_PR_SHIFT)) & PCC_CLKCFG_PR_MASK)
-
-/* The count of PCC_CLKCFG */
-#define PCC_CLKCFG_COUNT	(130U)
+/* The count of PCC clocks in one PCC block */
+#define PCC_NR_CLKS			130
+#define CLK_PCC0_BASE			0
+#define CLK_PCC1_BASE			(CLK_PCC0_BASE + PCC_NR_CLKS)
+#define CLK_PCC(reg)					\
+	((PCC_##reg <= PCC1_BASE) ?			\
+	 CLK_PCC0_BASE + ((PCC_##reg - PCC0_BASE) / 4) :\
+	 CLK_PCC1_BASE + ((PCC_##reg - PCC1_BASE) / 4))
 
 #endif /* __PCC_VEGA_H_INCLUDE__ */
