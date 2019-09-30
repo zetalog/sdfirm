17d49fc10bf2b99138e83b633e773bc9c895c33d
Lv Zheng <zhenglv@hotmail.com>
2019-09-30 09:13:48 +0800

    amba: Add AXI memory attributes
    To be used by duowen IMC_AT driver.
    
    Signed-off-by: Lv Zheng <zhenglv@hotmail.com>

--- arch/riscv/include/asm/mach-duowen/imc.h
+++ arch/riscv/include/asm/mach-duowen/imc.h
@@ -44,6 +44,7 @@
 
 #include <target/arch.h>
 #include <target/types.h>
+#include <target/amba.h>
 
 #define IMC_CSR_REG(offset)		(IMC_CSR_BASE + (offset))
 #define IMC_CSR_BOOT_ADDR		IMC_CSR_REG(0x00)
@@ -105,7 +106,7 @@
 #define IMC_AT_OUT_43_24_MASK		REG_20BIT_MASK
 #define IMC_AT_OUT_43_24(value)		_SET_FV(IMC_AT_OUT_43_24, value)
 
-/* IMC_AT attributes */
+/* IMC_AT attributes, see AXI_AXPROT/AXI_AXCACHE/AXBURST */
 #define IMC_AT_ATTR_PROT_OFFSET		0
 #define IMC_AT_ATTR_PROT_MASK		IMC_AT_PROT_MASK
 #define IMC_AT_ATTR_SET_PROT(value)	_SET_FV(IMC_AT_ATTR_PROT, value)
@@ -118,6 +119,10 @@
 #define IMC_AT_ATTR_BURST_MASK		IMC_AT_BURST_MASK
 #define IMC_AT_ATTR_SET_BURST(value)	_SET_FV(IMC_AT_ATTR_BURST, value)
 #define IMC_AT_ATTR_GET_BURST(value)	_GET_FV(IMC_AT_ATTR_BURST, value)
+#define IMC_AT_ATTR(burst, cache, prot)	\
+	(IMC_AT_ATTR_SET_BURST(burst) |	\
+	 IMC_AT_ATTR_SET_CACHE(cache) | \
+	 IMC_AT_ATTR_SET_PROT(prot))
 #define imc_sfab_set_valid(n)		\
 	__raw_setl(IMC_AT_VALID, IMC_ADDR_TRANS_HI(n));
 #define imc_sfab_set_invalid(n)		\
---
+++ include/target/amba.h
@@ -0,0 +1,63 @@
+/*
+ * ZETALOG's Personal COPYRIGHT
+ *
+ * Copyright (c) 2019
+ *    ZETALOG - "Lv ZHENG".  All rights reserved.
+ *    Author: Lv "Zetalog" Zheng
+ *    Internet: zhenglv@hotmail.com
+ *
+ * This COPYRIGHT used to protect Personal Intelligence Rights.
+ * Redistribution and use in source and binary forms with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *    This product includes software developed by the Lv "Zetalog" ZHENG.
+ * 3. Neither the name of this software nor the names of its developers may
+ *    be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ * 4. Permission of redistribution and/or reuse of souce code partially only
+ *    granted to the developer(s) in the companies ZETALOG worked.
+ * 5. Any modification of this software should be published to ZETALOG unless
+ *    the above copyright notice is no longer declaimed.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE ZETALOG AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE ZETALOG OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * @(#)amba.h: advanced microcontroller bus architecture (AMBA) definitions
+ * $Id: amba.h,v 1.1 2019-09-30 09:11:00 zhenglv Exp $
+ */
+
+#ifndef __AMBA_H_INCLUDE__
+#define __AMBA_H_INCLUDE__
+
+#include <target/types.h>
+
+#define AXI_AXPROT_PRIVILEDGED		_BV(0)
+#define AXI_AXPROT_NON_PRIVILEDGED	0
+#define AXI_AXPROT_NON_SECURE		_BV(1)
+#define AXI_AXPROT_SECURE		0
+#define AXI_AXPROT_INSTRUCTION		_BV(2)
+#define AXI_AXPROT_DATA			0
+
+#define AXI_AXCACHE_BUFFERABLE		_BV(0)
+#define AXI_AXCACHE_CACHEABLE		_BV(1)
+#define AXI_AXCACHE_READ_ALLOCATE	_BV(2)
+#define AXI_AXCACHE_WRITE_ALLOCATE	_BV(3)
+
+#define AXI_AXBURST_FIXED		0
+#define AXI_AXBURST_INCR		1
+#define AXI_AXBURST_WRAP		2
+
+#endif /* __AMBA_H_INCLUDE__ */
