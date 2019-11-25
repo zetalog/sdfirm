00bc9b48b418ae8993df5dc94d754736de3a9c0f
Lv Zheng <zhenglv@hotmail.com>
2019-11-25 13:24:32 +0800

    sunxi: Add options for fuse.sh
    This patch adds options for fuse.sh, so that we can use it to
    restore sunxi-spl.bin by:
     $ IMAGE=sunxi-spl.bin ./fuse.sh /dev/sdb
    
    Signed-off-by: Lv Zheng <zhenglv@hotmail.com>

--- arch/arm64/mach-sunxi/fuse.sh
+++ arch/arm64/mach-sunxi/fuse.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-sudo dd if=arch/arm64/boot/sdfirm.bin of=$1 bs=1024 seek=8
+SCRIPT=`(cd \`dirname $0\`; pwd)`
+SDFIRM=`(cd $SCRIPT/../../..; pwd)`
+
+if [ "x${IMAGE}" = "x" ]; then
+	IMAGE=${SDFIRM}/arch/arm64/boot/sdfirm.bin
+fi
+
+sudo dd if=$IMAGE of=$1 bs=1024 seek=8
