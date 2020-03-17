bb1c20c41eafc6e51e82b79f8aa7ca156136cebe
Lv Zheng <zhenglv@hotmail.com>
2020-03-17 11:10:34 +0800

    dpu: Update defconfig to split ROM/Flash
    This patch updates DPU defconfig to split ROM/Flash configuration.
    
    Signed-off-by: Lv Zheng <zhenglv@hotmail.com>

--- buildall.sh
+++ buildall.sh
@@ -44,7 +44,8 @@ build_sdfirm riscv32 duowen_fsbl
 build_sdfirm riscv64 duowen_bbl
 build_sdfirm riscv64 duowen_sv39
 build_sdfirm riscv64 duowen_sv48
-build_sdfirm riscv64 dpu_fsbl
+build_sdfirm riscv64 dpu_rom
+build_sdfirm riscv64 dpu_flash
 # disabled due to bin2vhx.pl bug
 #build_sdfirm riscv32 vega_ri5cy
 
