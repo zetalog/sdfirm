6bcc182b5318b161196b2305b4da8c87c0377f4e
Ian Jiang <ianjiang.ict@gmail.com>
2020-04-23 17:56:38 +0800

    spd: Add write API for SPD EE1004 driver
    Signed-off-by: Ian Jiang <ianjiang.ict@gmail.com>

--- drivers/spd/spd_ee1004.c
+++ drivers/spd/spd_ee1004.c
@@ -87,6 +87,75 @@ static int spd_ee_dummy_write(uint8_t dev, uint8_t addr)
 	return 0;
 }
 
+#if 0
+static int spd_ee_byte_write(uint8_t dev, uint8_t addr, uint8_t data)
+{
+	uint8_t dsc;
+	uint8_t buf[] = {addr, data};
+	int ret;
+
+	dsc = SPD_DSC_WSPD | (dev << 1);
+#ifdef SPD_EE1004_DEBUG
+	con_printf("Debug: Do dummy wirte. dsc = 0x%x, addr = 0x%x, data = 0x%x\n", dsc, addr, data);
+#endif
+	ret = i2c_hw_write_bytes((dsc >> 1), buf, sizeof(buf), 1);
+	if (ret != 1) {
+		con_printf("Error: Failed to do byte wirte. dsc = 0x%x, ret = %d\n", dsc, ret);
+		return ret;
+	}
+	return 1;
+}
+#endif
+
+static int spd_ee_page_write(uint8_t dev, uint8_t addr, uint8_t *data, int len)
+{
+	uint8_t dsc;
+	int i2c_tx_len;
+	uint8_t i2c_tx_buf[SPD_EE_PAGE_WRITE_SIZE + 1] = {0};
+	int ret;
+
+	if (data == NULL) return -1;
+	if (len <= 0 || len > SPD_EE_PAGE_WRITE_SIZE) return -1;
+	if ((addr + len) > SPD_EE_PAGE_SIZE) return -1;
+
+	dsc = SPD_DSC_WSPD | (dev << 1);
+	i2c_tx_buf[0] = addr;
+	for (int i = 0; i < len; i++) {
+		i2c_tx_buf[i + 1] = data[i];
+	}
+	i2c_tx_len = len + 1;
+#ifdef SPD_EE1004_DEBUG
+	con_printf("Debug: Do page wirte. dsc = 0x%x, addr = 0x%x, len = 0x%x\n", dsc, addr, len);
+#endif
+	ret = i2c_hw_write_bytes((dsc >> 1), i2c_tx_buf, i2c_tx_len, 1);
+	if (ret != i2c_tx_len) {
+		con_printf("Error: Failed to do page wirte. dsc = 0x%x, ret = %d\n", dsc, ret);
+		return ret;
+	}
+	return len;
+}
+
+static int spd_ee_write_in_page(uint8_t dev, int page, uint8_t addr, uint8_t *data, int len)
+{
+	int tx_len = 0;
+	if (data == NULL) return -1;
+
+	if (page == 0)
+		spd_ee_set_page(0);
+	else
+		spd_ee_set_page(1);
+	while (tx_len < len) {
+		int this_len;
+		if ((len - tx_len) > SPD_EE_PAGE_WRITE_SIZE)
+			this_len = SPD_EE_PAGE_WRITE_SIZE;
+		else
+			this_len = len - tx_len;
+		spd_ee_page_write(dev, (addr + tx_len), (data + tx_len), this_len);
+		tx_len += this_len;
+	}
+	return len;
+}
+
 int spd_hw_read_bytes(uint8_t dev, uint16_t addr, uint8_t *buffer, int len)
 {
 	uint8_t dsc;
@@ -159,6 +228,13 @@ int spd_hw_read_bytes(uint8_t dev, uint16_t addr, uint8_t *buffer, int len)
 
 int spd_hw_write_bytes(uint8_t dev, uint16_t addr, uint8_t *buffer, int len)
 {
+	uint8_t dsc;
+	int len_in_page_0 = 0;
+	int len_in_page_1 = 0;
+	int this_len;
+	uint8_t this_addr;
+	int ret;
+
 	if (buffer == NULL) {
 		con_printf("Error: Invalid buffer = %p\n", buffer);
 		return -1;
@@ -167,10 +243,46 @@ int spd_hw_write_bytes(uint8_t dev, uint16_t addr, uint8_t *buffer, int len)
 		con_printf("Error: Invalid SPD num = %u\n", dev);
 		return -1;
 	}
-	if (addr >= SPD_EE_SIZE || (addr + len) >= SPD_EE_SIZE) {
+	if (addr >= SPD_EE_SIZE || (addr + len) > SPD_EE_SIZE) {
 		con_printf("Error: Invalid EE addr = 0x%x, len = %d\n", addr, len);
 		return -1;
 	}
 
-	return 0;
+	dsc = SPD_DSC_WSPD | (dev << 1);
+
+	if (addr + len < SPD_EE_PAGE_SIZE) {
+		len_in_page_0 = len;
+		len_in_page_1 = 0;
+	} else if (addr > SPD_EE_PAGE_SIZE) {
+		len_in_page_0 = 0;
+		len_in_page_1 = len;
+	} else {
+		len_in_page_0 = SPD_EE_PAGE_SIZE - addr;
+		len_in_page_1 = len - len_in_page_0;
+	}
+
+	if (len_in_page_0 > 0) {
+		this_len = len_in_page_0;
+		this_addr = (uint8_t)addr;
+		ret = spd_ee_write_in_page(dev, 0, this_addr, buffer, this_len);
+		if (ret < 0) {
+			con_printf("Error: Failed to write EE page 0. dsc = 0x%x, addr = 0x%x, len = %d, ret = %d\n", dsc, this_addr, this_len, ret);
+			return -1;
+		}
+	}
+
+	if (len_in_page_1 > 0) {
+		this_len = len_in_page_1;
+		if (addr < SPD_EE_PAGE_SIZE) {
+			this_addr = 0;
+		} else {
+			this_addr = (uint8_t)(addr - SPD_EE_PAGE_SIZE);
+		}
+		ret = spd_ee_write_in_page(dev, 1, this_addr, buffer, this_len);
+		if (ret < 0) {
+			con_printf("Error: Failed to write EE page 1. dsc = 0x%x, addr = 0x%x, len = %d, ret = %d\n", dsc, this_addr, this_len, ret);
+			return -1;
+		}
+	}
+	return len;
 }
