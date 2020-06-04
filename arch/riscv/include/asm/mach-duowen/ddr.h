661a49e38e484998bb1f91e90c2a3aaf1f8f5284
Lv Zheng <zhenglv@hotmail.com>
2020-06-04 16:56:22 +0800

    Add BBL payload support
    This patch adds SBI payload support to allow sdfirm to be built
    as BBL, however FDT support is not complete in this patch.
    
    Signed-off-by: Lv Zheng <zhenglv@hotmail.com>

---
+++ arch/riscv/include/asm/mach-duowen/ddr.h
@@ -0,0 +1,65 @@
+/*
+ * ZETALOG's Personal COPYRIGHT
+ *
+ * Copyright (c) 2020
+ *    ZETALOG - "Lv ZHENG".  All rights reserved.
+ *    Author: Lv "Zetalog" Zheng
+ *    Internet: zhenglv@hotmail.com
+ *
+ * This COPYRIGHT used to protect Personal Intelligence Rights.
+ * Redistribution and use in source and binary forms with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *    This product includes software developed by the Lv "Zetalog" ZHENG.
+ * 3. Neither the name of this software nor the names of its developers may
+ *    be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ * 4. Permission of redistribution and/or reuse of souce code partially only
+ *    granted to the developer(s) in the companies ZETALOG worked.
+ * 5. Any modification of this software should be published to ZETALOG unless
+ *    the above copyright notice is no longer declaimed.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE ZETALOG AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE ZETALOG OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * @(#)ddr.h: DPU specific DDR definitions
+ * $Id: ddr.h,v 1.1 2020-03-26 08:55:00 zhenglv Exp $
+ */
+
+#ifndef __DDR_DUOWEN_H_INCLUDE__
+#define __DDR_DUOWEN_H_INCLUDE__
+
+#include <target/arch.h>
+#include <target/clk.h>
+
+#ifdef CONFIG_DUOWEN_DDR
+#ifndef ARCH_HAVE_DDR
+#define ARCH_HAVE_DDR		1
+#else
+#error "Multiple DDR controller defined"
+#endif
+#endif
+
+#include <driver/dw_umctl2.h>
+
+#define NR_DDR_SPEEDS		(DDR4_3200 + 1)
+
+#ifdef CONFIG_DUOWEN_DDR
+/* DDR frequency plans */
+#define ddr_hw_ctrl_init()	dw_umctl2_init()
+#endif
+
+#endif /* __DDR_DUOWEN_H_INCLUDE__ */
--- buildall.sh
+++ buildall.sh
@@ -38,19 +38,24 @@ build_sdfirm arm64 ddr_umctl2_tb
 # disabled due to spl size limitation
 #build_sdfirm arm64 nanopi_neo2_spl
 
-build_sdfirm riscv32 spike_rv32
+build_sdfirm riscv32 spike32_tb
+build_sdfirm riscv32 vega_ri5cy
+
+build_sdfirm riscv64 spike64_tb
+build_sdfirm riscv64 spike64_bbl
+build_sdfirm riscv64 virt_rv64
+
+build_sdfirm riscv64 unleashed_fsbl
+build_sdfirm riscv64 unleashed_bbl
+build_sdfirm riscv64 k210_bbl
+
+# DUOWEN
 build_sdfirm riscv32 duowen_zsbl
 build_sdfirm riscv32 duowen_fsbl
 build_sdfirm riscv64 duowen_bbl
 build_sdfirm riscv64 duowen_sv39
 build_sdfirm riscv64 duowen_sv48
-build_sdfirm riscv32 dpu_rom
-build_sdfirm riscv32 dpu_flash
-build_sdfirm riscv32 dpu_ram
-build_sdfirm riscv32 vega_ri5cy
-
-build_sdfirm riscv64 spike_rv64
-build_sdfirm riscv64 virt_rv64
-build_sdfirm riscv64 unleashed_fsbl
-build_sdfirm riscv64 unleashed_bbl
-build_sdfirm riscv64 k210_bbl
+# DPU
+build_sdfirm riscv64 dpu_rom
+build_sdfirm riscv64 dpu_flash
+build_sdfirm riscv64 dpu_ram
