e9f231bb757373995837f0dfda0f7b6cc396b4dc
Lv Zheng <zhenglv@hotmail.com>
2020-06-16 23:51:55 +0800

    scripts: Add dts dumper to runners
    This patch adds DTS dumper support in run-spike/qemu.sh.
    
    Signed-off-by: Lv Zheng <zhenglv@hotmail.com>

--- scripts/run-qemu.sh
+++ scripts/run-qemu.sh
@@ -1,8 +1,54 @@
 #!/bin/sh
+#
+# Run QEMU virtio platform
+#
+# This script is used to run qemu
+
+SCRIPT=`(cd \`dirname $0\`; pwd)`
+
+QEMU_OPTS=-nographic
+QEMU_MOPTS=
+
+usage()
+{
+	echo "Usage:"
+	echo "`basename $0` [-p procs] [-t file]"
+	echo "Where:"
+	echo " -p num-cpus: specify number of CPUs"
+	echo " -t dts-file: dump device tree string file"
+	exit $1
+}
+
+fatal_usage()
+{
+	echo $1
+	usage 1
+}
+
+while getopts "hp:t:" opt
+do
+	case $opt in
+	h) usage 0;;
+	p) QEMU_OPTS="-smp $OPTARG ${QEMU_OPTS}";;
+	t) QEMU_DTS=$OPTARG;;
+	?) echo "Invalid argument $opt"
+	   fatal_usage;;
+	esac
+done
+shift $(($OPTIND - 1))
 
 SCRIPT=`(cd \`dirname $0\`; pwd)`
 
 ARCH=riscv64
 QEMU=qemu-system-${ARCH}
 
-${QEMU} -nographic -machine virt -bios ${SCRIPT}/../sdfirm
+if [ "x${QEMU_DTS}" != "x" ]; then
+	QEMU_DTB="`echo ${QEMU_DTS%.*}`.dtb"
+	QEMU_MOPTS=",dumpdtb=${QEMU_DTB}"
+fi
+
+${QEMU} ${QEMU_OPTS} -machine virt${QEMU_MOPTS} -bios ${SCRIPT}/../sdfirm
+
+if [ "x${QEMU_DTS}" != "x" ]; then
+	dtc -I dtb -O dts -o ${QEMU_DTS} ${QEMU_DTB}
+fi
--- scripts/run-spike.sh
+++ scripts/run-spike.sh
@@ -9,14 +9,19 @@ SCRIPT=`(cd \`dirname $0\`; pwd)`
 
 SPIKE_OPTS=
 SPIKE_RBB_PORT=9824
+SPIKE_RBB=yes
+SPIKE_PIPE=
 
 usage()
 {
 	echo "Usage:"
-	echo "`basename $0` [-p procs] [-j port] [-H]"
+	echo "`basename $0` [-d] [-j port] [-l] [-p procs] [-t file] [-H]"
 	echo "Where:"
-	echo " -p num-cpus: specify number of CPUs"
+	echo " -d:          enable single step debug"
+	echo " -l:          enable execution log"
 	echo " -j rbb-port: specify jtag remote bitbang port (default 9824)"
+	echo " -p num-cpus: specify number of CPUs"
+	echo " -t dts-file: dump device tree string file"
 	echo " -H:          enable halt on reset"
 	exit $1
 }
@@ -27,16 +32,28 @@ fatal_usage()
 	usage 1
 }
 
-while getopts "j:p:H" opt
+while getopts "dhj:lp:t:H" opt
 do
 	case $opt in
-	j) SPIKE_RBB_PORT=$OPTARG;;
+	d) SPIKE_OPTS="${SPIKE_OPTS} -d";;
+	h) usage 0;;
+	j) SPIKE_RBB_PORT=$OPTARG
+	   SPIKE_RBB=yes;;
+	l) SPIKE_OPTS="${SPIKE_OPTS} -l";;
 	p) SPIKE_OPTS="-p$OPTARG ${SPIKE_OPTS}";;
-	H) SPIKE_OPTS="${SPIKE_OPTS} -H";;
+	t) SPIKE_OPTS="--dump-dts ${SPIKE_OPTS}"
+	   SPIKE_PIPE=">$OPTARG"
+	   SPIKE_RBB=no;;
+	H) SPIKE_OPTS="${SPIKE_OPTS} -H"
+	   SPIKE_RBB=yes;;
 	?) echo "Invalid argument $opt"
 	   fatal_usage;;
 	esac
 done
 shift $(($OPTIND - 1))
 
-spike ${SPIKE_OPTS} --rbb-port=${SPIKE_RBB_PORT} ${SCRIPT}/../sdfirm
+if [ "x${SPIKE_RBB}" = "xyes" ]; then
+	SPIKE_OPTS="--rbb-port=${SPIKE_RBB_PORT} ${SPIKE_OPTS}"
+fi
+
+eval spike ${SPIKE_OPTS} ${SCRIPT}/../sdfirm ${SPIKE_PIPE}
