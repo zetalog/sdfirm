zetalog/sdfirm

commit d1c53b6cd63b44f173da2ed8eb2a365f000ba1c2

bench: Fix SMP first run issue
When COMMAND_BATCH is y, we can only see first core running bench
tests, while all the other cores are silient. This is because we
won't wait other cores to boot on boot core, and smp bench command
will fail to wake up other cores by bh_resume_smp().
This patch adds CONFIG_SMP_WAIT_BOOT to fix the issue.

Signed-off-by: Lv Zheng <zhenglv@hotmail.com>

kernel/smp.c
@@ -49,6 +49,7 @@
 #include <target/bench.h>
 #include <target/atomic.h>
 #include <target/paging.h>
+#include <target/cmdline.h>
 
 cpu_t smp_boot_cpu;
 cpu_mask_t smp_online_cpus;
@@ -72,14 +73,17 @@ void smp_init(void)
 		timer_init();
 		task_init();
 		bench_init();
-		bh_loop();
 	} else {
 		cpu_t cpu;
 
 		for (cpu = 0; cpu < NR_CPUS; cpu++) {
-			if (cpu != smp_boot_cpu)
+			if (cpu != smp_boot_cpu) {
 				smp_cpu_on(cpu, (caddr_t)smp_init);
+				while (!cpumask_test_cpu(cpu, &smp_online_cpus));
+			}
 		}
 		bench_init();
+		cmd_init();
 	}
+	bh_loop();
 }

init/main.c
@@ -94,9 +94,6 @@ void system_init(void)
 	board_late_init();
 	appl_init();
 	smp_init();
-	bench_init();
-	cmd_init();
-	bh_loop();
 }
 
 void system_suspend(void)

kernel/Kconfig
@@ -159,6 +159,9 @@ if SMP
 config ARCH_HAS_QSPINLOCK
 	bool
 
+config SMP_WAIT_BOOT
+	bool "Wait other CPUs to boot up"
+
 endif
 
 menu "Timing facility (jiffy) support"

tests/Kconfig
@@ -40,7 +40,7 @@ config TEST_BENCH_DIDT
 	  tests.
 
 config TEST_BENCH_START_DELAY
-	bool "Start delay alignment"
+	int "Start delay alignment"
 	depends SMP
 	default 64
 	help

tests/bench.c
@@ -257,7 +257,7 @@ static void bench_reset_timeout(void)
 	cpu_t cpu = smp_processor_id();
 	tick_t tick = tick_get_counter();
 
-	if (time_after(tick, cpu_ctxs[cpu].async_timeout))
+	if (!time_before(tick, cpu_ctxs[cpu].async_timeout))
 		bench_raise_event(CPU_EVENT_TIME);
 	else
 		timer_schedule_shot(cpu_ctxs[cpu].timer,

commit 81e22e472527304fd40a79ff6920951f4803bea2

smp: Fix regression of missing bench_init()
This patch fixes regression introduced in d1c53b6cd63b44, all
bench_init/cmd_init stuffs are moved to the smp_init() to ensure
there's only one invocation for them in SMP environment.

This patch restores the missing stuffs back for SMP=n environment.

Signed-off-by: Lv Zheng <zhenglv@hotmail.com>

arch/riscv/configs/virt64_bbl_defconfig
@@ -1,7 +1,7 @@
 #
 # Automatically generated make config: don't edit
 # Small Device version: 1.0.0.0
-# Tue Jun 16 12:55:11 2020
+# Wed Jun 17 09:25:39 2020
 #
 CONFIG_64BIT=y
 CONFIG_RISCV=y
@@ -10,7 +10,7 @@ CONFIG_RISCV=y
 # Generic setup
 #
 CONFIG_VENDOR_NAME="SmartCore"
-CONFIG_PRODUCT_NAME="QEMU Virt RISCV64 BBL"
+CONFIG_PRODUCT_NAME="QEMU RISCV64 (VIRT) Berkeley Bootloader"
 CONFIG_VENDOR_ID=990
 CONFIG_PRODUCT_ID=0
 CONFIG_SERIAL_NO=0
@@ -29,7 +29,7 @@ CONFIG_CONSOLE=y
 CONFIG_CONSOLE_COMMAND=y
 CONFIG_CONSOLE_OUTPUT=y
 CONFIG_CONSOLE_OUTPUT_CR=y
-CONFIG_CONSOLE_PRINT_BUFFER_SIZE=256
+CONFIG_CONSOLE_PRINT_BUFFER_SIZE=512
 CONFIG_CONSOLE_INPUT=y
 # CONFIG_CONSOLE_230400 is not set
 CONFIG_CONSOLE_115200=y

arch/riscv/configs/virt64_tb_defconfig
@@ -1,7 +1,7 @@
 #
 # Automatically generated make config: don't edit
 # Small Device version: 1.0.0.0
-# Tue Jun 16 13:23:09 2020
+# Wed Jun 17 09:23:21 2020
 #
 CONFIG_64BIT=y
 CONFIG_RISCV=y
@@ -10,7 +10,7 @@ CONFIG_RISCV=y
 # Generic setup
 #
 CONFIG_VENDOR_NAME="SmartCore"
-CONFIG_PRODUCT_NAME="QEMU Virt RISCV64 BBL"
+CONFIG_PRODUCT_NAME="QEMU RISCV64 (VIRT) Test Bench"
 CONFIG_VENDOR_ID=990
 CONFIG_PRODUCT_ID=0
 CONFIG_SERIAL_NO=0
@@ -29,7 +29,7 @@ CONFIG_CONSOLE=y
 CONFIG_CONSOLE_COMMAND=y
 CONFIG_CONSOLE_OUTPUT=y
 CONFIG_CONSOLE_OUTPUT_CR=y
-CONFIG_CONSOLE_PRINT_BUFFER_SIZE=256
+CONFIG_CONSOLE_PRINT_BUFFER_SIZE=512
 CONFIG_CONSOLE_INPUT=y
 # CONFIG_CONSOLE_230400 is not set
 CONFIG_CONSOLE_115200=y

include/target/smp.h
@@ -74,8 +74,6 @@ extern cpu_t smp_boot_cpu;
 extern cpu_mask_t smp_online_cpus;
 extern bool smp_initialized;
 
-void smp_init(void);
-
 #define cpumask_bits(maskp)		((maskp)->bits)
 #define cpumask_set_cpu(cpu, maskp)	set_bit((cpu), cpumask_bits(maskp))
 #define cpumask_clear_cpu(cpu, maskp)	clear_bit((cpu), cpumask_bits(maskp))
@@ -101,10 +99,13 @@ extern cpu_mask_t smp_online_cpus;
 #define cpumask_clear_cpu(cpu, maskp)	(*(maskp) = 0)
 #define cpumask_test_cpu(cpu, maskp)	(C(cpu) == *(maskp))
 
-#define smp_init()			do { } while (0)
 #define smp_cpu_on(cpu, ep, context)	do { } while (0)
 #endif /* __ASSEMBLY__ */
 #endif
 #define __cache_aligned			__align(SMP_CACHE_BYTES)
+
+#ifndef __ASSEMBLY__
+void smp_init(void);
+#endif /* __ASSEMBLY__ */
 
 #endif /* __SMP_H_INCLUDE__ */

kernel/Makefile
@@ -9,7 +9,7 @@ obj-y += bh.rel
 obj-y += irq.rel
 obj-y += delay.rel
 obj-y += stack.rel
-obj-$(CONFIG_SMP) += smp.rel
+obj-y += smp.rel
 obj-$(CONFIG_SMP) += percpu.rel
 obj-$(CONFIG_ARCH_HAS_QSPINLOCK) += spinlock.rel
 obj-$(CONFIG_MEM) += mem.rel

kernel/smp.c
@@ -51,6 +51,7 @@
 #include <target/paging.h>
 #include <target/cmdline.h>
 
+#ifdef CONFIG_SMP
 cpu_t smp_boot_cpu;
 cpu_mask_t smp_online_cpus;
 
@@ -87,3 +88,11 @@ void smp_init(void)
 	}
 	bh_loop();
 }
+#else
+void smp_init(void)
+{
+	bench_init();
+	cmd_init();
+	bh_loop();
+}
+#endif