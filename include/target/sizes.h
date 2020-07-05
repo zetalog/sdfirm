5ec6e8083d2c0cf4c8d3cfd5e5b606654d8bba3b
Lv Zheng <zhenglv@hotmail.com>
2020-07-05 23:47:52 +0800

    Extend register definition macros
    This patch adds new register definition macros to allow define
    register fields for 64-bit registers.
    
    Signed-off-by: Lv Zheng <zhenglv@hotmail.com>

--- include/target/sizes.h
+++ include/target/sizes.h
@@ -40,5 +40,17 @@
 #define SZ_2G				0x80000000
 
 #define SZ_4G				_AC(0x100000000, ULL)
+#define SZ_8G				_AC(0x200000000, ULL)
+#define SZ_16G				_AC(0x400000000, ULL)
+#define SZ_32G				_AC(0x800000000, ULL)
+#define SZ_64G				_AC(0x1000000000, ULL)
+#define SZ_128G				_AC(0x2000000000, ULL)
+#define SZ_256G				_AC(0x4000000000, ULL)
+#define SZ_512G				_AC(0x8000000000, ULL)
+
+#define SZ_1T				_AC(0x10000000000, ULL)
+#define SZ_2T				_AC(0x20000000000, ULL)
+#define SZ_4T				_AC(0x40000000000, ULL)
+#define SZ_8T				_AC(0x80000000000, ULL)
 
 #endif /* __TARGET_SIZES_H_INCLUDE__ */
