3699c9e63d8a4589d403b0fa5cffd12b07aad3d5
Lv Zheng <zhenglv@hotmail.com>
2020-07-29 09:53:22 +0800

    vaisra: Add L2 error report support
    This patch adds L2 error reporting support.
    
    Signed-off-by: Lv Zheng <zhenglv@hotmail.com>

--- arch/riscv/common/vaisra_ras.c
+++ arch/riscv/common/vaisra_ras.c
@@ -23,26 +23,85 @@ static struct vaisra_err vaisra_ices[MAX_ICES] = {
 	{ ICE_V1ICPEO,   "Interleave-1 I-cache parity error observed" },
 };
 
+static struct vaisra_err vaisra_l2es[MAX_L2ES] = {
+	{ L2E_TSBEC0AD,  "Tag single bit error cpu0 abort or discard" },
+	{ L2E_TSBEC0NA,  "Tag single bit error cpu0 no-abort" },
+	{ L2E_TSBEC1AD,  "Tag single bit error cpu1 abort or discard" },
+	{ L2E_TSBEC1NA,  "Tag single bit error cpu1 no-abort" },
+	{ L2E_TSBESNP,   "Tag single bit error snoop" },
+	{ L2E_TSBEL2I,   "Tag single bit error L2 initiated" },
+	{ L2E_TDBEC0AD,  "Tag double bit error cpu0 abort or discard" },
+	{ L2E_TDBEC0NA,  "Tag double bit error cpu0 no-abort" },
+	{ L2E_TDBEC1AD,  "Tag double bit error cpu1 abort or discard" },
+	{ L2E_TDBEC1NA,  "Tag double bit error cpu1 no-abort" },
+	{ L2E_TDBESNP,   "Tag double bit error snoop" },
+	{ L2E_TDBEL2I,   "Tag double bit error L2 initiated" },
+	{ L2E_DSBEC0AD,  "Data single bit error cpu0 abort or discard" },
+	{ L2E_DSBEC0NA,  "Data single bit error cpu0 no-abort" },
+	{ L2E_DSBEC1AD,  "Data single bit error cpu1 abort or discard" },
+	{ L2E_DSBEC1NA,  "Data single bit error cpu1 no-abort" },
+	{ L2E_DSBESNP,   "Data single bit error snoop" },
+	{ L2E_DSBEL2I,   "Data single bit error L2 initiated" },
+	{ L2E_DDBEC0AD,  "Data double bit error cpu0 abort or discard" },
+	{ L2E_DDBEC0NA,  "Data double bit error cpu0 no-abort" },
+	{ L2E_DDBEC1AD,  "Data double bit error cpu1 abort or discard" },
+	{ L2E_DDBEC1NA,  "Data double bit error cpu1 no-abort" },
+	{ L2E_DDBESNP,   "Data double bit error snoop" },
+	{ L2E_DDBEL2I,   "Data double bit error L2 initiated" },
+	{ L2E_BMPCEC0AD, "Bus master port command error cpu0 abort or discard" },
+	{ L2E_BMPCEC0NA, "Bus master port command error cpu0 no-abort" },
+	{ L2E_BMPCEC1AD, "Bus master port command error cpu1 abort or discard" },
+	{ L2E_BMPCEC1NA, "Bus master port command error cpu1 no-abort" },
+	{ L2E_BMPCESNP,  "Bus master port command error snoop" },
+	{ L2E_BMPCEL2I,  "Bus master port command error L2 initiated" },
+	{ L2E_BMPDEC0AD, "Bus master port data error cpu0 abort or discard" },
+	{ L2E_BMPDEC0NA, "Bus master port data error cpu0 no-abort" },
+	{ L2E_BMPDEC1AD, "Bus master port data error cpu1 abort or discard" },
+	{ L2E_BMPDEC1NA, "Bus master port data error cpu1 no-abort" },
+};
+
+void l2csr_write(uint16_t csr, uint64_t val)
+{
+	csr_write(CSR_ML2CSRS, csr);
+	csr_write(CSR_ML2CSRD, val);
+}
+
+uint64_t l2csr_read(uint16_t csr)
+{
+	csr_write(CSR_ML2CSRS, csr);
+	return csr_read(CSR_ML2CSRD);
+}
+
 #ifdef CONFIG_SBI
 void sbi_process_vaisra_nmi(void)
 {
-	uint32_t ecr, esr;
+	uint64_t ecr0, ecr1, esr;
 	int err;
 
 	sbi_printf("IRQ_M_NMI\n");
 
-	ecr = csr_read(CSR_DCEC);
-	esr = csr_read(CSR_DCES);
+	ecr0 = csr_read(CSR_MDCEC);
+	esr = csr_read(CSR_MDCES);
 	for (err = 0; err < MAX_DCES; err++) {
-		if (err_indicated(err) && err_nmi_enabled(err))
+		if (err_indicated(esr, err) && err_nmi_enabled(ecr0, err))
 			printf("%s\n", vaisra_dces[err].verbose);
 	}
 
-	ecr = csr_read(CSR_ICEC);
-	esr = csr_read(CSR_ICES);
+	ecr0 = csr_read(CSR_MICEC);
+	esr = csr_read(CSR_MICES);
 	for (err = 0; err < MAX_DCES; err++) {
-		if (err_indicated(err) && err_nmi_enabled(err))
+		if (err_indicated(esr, err) && err_nmi_enabled(ecr0, err))
 			printf("%s\n", vaisra_ices[err].verbose);
+	}
+
+	ecr0 = l2csr_read(L2CSR_L2EC0);
+	ecr1 = l2csr_read(L2CSR_L2EC1);
+	esr = l2csr_read(L2CSR_L2ES_GC);
+	for (err = 0; err < MAX_L2ES; err++) {
+		if (err_indicated(esr, err) && (err > 31 ?
+		    err_nmi_enabled(ecr1, err - 32) :
+		    err_nmi_enabled(ecr0, err)))
+			printf("%s\n", vaisra_l2es[err].verbose);
 	}
 }
 #endif /* CONFIG_SBI */
--- arch/riscv/include/asm/vaisra_ras.h
+++ arch/riscv/include/asm/vaisra_ras.h
@@ -45,10 +45,14 @@
 #define IRQ_M_NMI	16
 
 /* dcache and icache, accessed via CSR */
-#define CSR_DCEC	0x5cd /* D-cache error control */
-#define CSR_DCES	0x5ce /* D-cache error status */
-#define CSR_ICEC	0x5d0 /* I-cache error control */
-#define CSR_ICES	0x5d1 /* I-cache error status */
+#define CSR_MDCEC	0x5cd /* D-cache error control */
+#define CSR_MDCES	0x5ce /* D-cache error status */
+#define CSR_MICEC	0x5d0 /* I-cache error control */
+#define CSR_MICES	0x5d1 /* I-cache error status */
+
+/* L2 indirect register access CSR */
+#define CSR_ML2CSRS	0x5d2 /* L2 CSR select register */
+#define CSR_ML2CSRD	0x5d3 /* L2 CSR data register */
 
 /* L2, accessed via L2CSR */ 
 #define L2CSR_L2EC0	0x208 /* L2-cache error control 0 */
@@ -70,25 +74,70 @@
 #define ICE_L0DPE	1 /* L0-icache data parity error */
 #define ICE_L1TPE	2 /* L1-icache tag parity error */
 #define ICE_L1DPE	3 /* L1-icache data parity error */
-#define ICE_V0ICPEO	4 /* interleave-0 I-cache parity error observed */
-#define ICE_V1ICPEO	5 /* interleave-1 I-cache parity error observed */
+#define ICE_V0ICPEO	4 /* Interleave-0 I-cache parity error observed */
+#define ICE_V1ICPEO	5 /* Interleave-1 I-cache parity error observed */
 
 #define MAX_ICES	6
 
+/* L2CSR */
+#define L2CSR_CLS	_AC(0x00000700, UL)
+#define L2CSR_REG	_AC(0x000000ff, UL)
+
 /* L2EC/L2ES */
+#define L2E_TSBEC0AD	0  /* Tag single bit error cpu0 abort or discard */
+#define L2E_TSBEC0NA	1  /* Tag single bit error cpu0 no-abort */
+#define L2E_TSBEC1AD	2  /* Tag single bit error cpu1 abort or discard */
+#define L2E_TSBEC1NA	3  /* Tag single bit error cpu1 no-abort */
+#define L2E_TSBESNP	4  /* Tag single bit error snoop */
+#define L2E_TSBEL2I	5  /* Tag single bit error L2 initiated */
+#define L2E_TDBEC0AD	6  /* Tag double bit error cpu0 abort or discard */
+#define L2E_TDBEC0NA	7  /* Tag double bit error cpu0 no-abort */
+#define L2E_TDBEC1AD	8  /* Tag double bit error cpu1 abort or discard */
+#define L2E_TDBEC1NA	9  /* Tag double bit error cpu1 no-abort */
+#define L2E_TDBESNP	10 /* Tag double bit error snoop */
+#define L2E_TDBEL2I	11 /* Tag double bit error L2 initiated */
+#define L2E_DSBEC0AD	12 /* Data single bit error cpu0 abort or discard */
+#define L2E_DSBEC0NA	13 /* Data single bit error cpu0 no-abort */
+#define L2E_DSBEC1AD	14 /* Data single bit error cpu1 abort or discard */
+#define L2E_DSBEC1NA	15 /* Data single bit error cpu1 no-abort */
+#define L2E_DSBESNP	16 /* Data single bit error snoop */
+#define L2E_DSBEL2I	17 /* Data single bit error L2 initiated */
+#define L2E_DDBEC0AD	18 /* Data double bit error cpu0 abort or discard */
+#define L2E_DDBEC0NA	19 /* Data double bit error cpu0 no-abort */
+#define L2E_DDBEC1AD	20 /* Data double bit error cpu1 abort or discard */
+#define L2E_DDBEC1NA	21 /* Data double bit error cpu1 no-abort */
+#define L2E_DDBESNP	22 /* Data double bit error snoop */
+#define L2E_DDBEL2I	23 /* Data double bit error L2 initiated */
+#define L2E_BMPCEC0AD	24 /* Bus master port command error cpu0 abort or discard */
+#define L2E_BMPCEC0NA	25 /* Bus master port command error cpu0 no-abort */
+#define L2E_BMPCEC1AD	26 /* Bus master port command error cpu1 abort or discard */
+#define L2E_BMPCEC1NA	27 /* Bus master port command error cpu1 no-abort */
+#define L2E_BMPCESNP	28 /* Bus master port command error snoop */
+#define L2E_BMPCEL2I	29 /* Bus master port command error L2 initiated */
+#define L2E_BMPDEC0AD	30 /* Bus master port data error cpu0 abort or discard */
+#define L2E_BMPDEC0NA	31 /* Bus master port data error cpu0 no-abort */
+#define L2E_BMPDEC1AD	32 /* Bus master port data error cpu1 abort or discard */
+#define L2E_BMPDEC1NA	33 /* Bus master port data error cpu1 no-abort */
+
+#define MAX_L2ES	34
 
 #define ERR_S(err)	_BV(err)
 #define ERR_ESEI(err)	_BV(((err) << 1) + 1)
 #define ERR_ENMI(err)	_BV((err) << 1)
 
+#ifndef __ASSEMBLER__
+void l2csr_write(uint16_t csr, uint64_t val);
+uint64_t l2csr_read(uint16_t csr);
+
 #define err_nmi_enabled(csr, err)		(!!((csr) & ERR_ENMI(err)))
 #define err_irq_enabled(csr, err)		(!!((csr) & ERR_ESEI(err)))
-#define err_indicated(csr, err)			(!!((csr) & ERR_E(err)))
+#define err_indicated(csr, err)			(!!((csr) & ERR_S(err)))
 
 #if defined(CONFIG_VAISRA_RAS) && defined(CONFIG_SBI)
 void sbi_process_vaisra_nmi(void);
 #else
 #define sbi_process_vaisra_nmi()	do { } while (0)
 #endif
+#endif /* __ASSEMBLER__ */
 
 #endif /* __RAS_VAISRA_H_INCLUDE__ */
