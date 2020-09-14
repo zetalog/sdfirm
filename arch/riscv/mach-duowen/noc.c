eabec378f1c86cebcfe901623e08e7fb156f2c19
Lv Zheng <zhenglv@hotmail.com>
2020-09-14 22:06:36 +0800

    Fix NoC initialization requirement
    This patch fixes NoC initialization requirement of ddr_rst.
    
    Signed-off-by: Lv Zheng <zhenglv@hotmail.com

--- arch/riscv/mach-duowen/noc.c
+++ arch/riscv/mach-duowen/noc.c
@@ -45,8 +45,11 @@
 
 void duowen_noc_init(void)
 {
-	/* DDR clock must be enabled before configuring NoC */
+	/* DDR clock must be enabled and DDR reset must be released
+	 * before configuring NoC.
+	 */
 	board_init_clock();
 	clk_enable(ddr_clk);
+	clk_enable(ddr_rst);
 	ncore_init(MAX_CPU_CLUSTERS, 0, MAX_DDR_SEGMENTS, MAX_DDR_SEGMENTS);
 }
