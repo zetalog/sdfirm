d6c62341971c12c4b01a6988de7ae019adb7a576
Lv Zheng <zhenglv@hotmail.com>
2020-12-16 13:29:08 +0800

    duowen: Cleanup GPT image maker
    This patch cleans up GPT image maker.
    
    Signed-off-by: Lv Zheng <zhenglv@smart-core.cn>

--- scripts/mkimg.sh
+++ scripts/mkimg.sh
@@ -5,11 +5,12 @@
 # Default:
 #   mkimg.sh ./img-root ./gpt.img
 
+SCRIPT=`(cd \`dirname $0\`; pwd)`
 #
 # User parameter
 # 
-input_dir=./img-root
-output_img=./gpt.img
+input_dir=${SCRIPT}/img-root
+output_img=${SCRIPT}/gpt.img
 if [[ $# -gt 0 ]]
 then
 	input_dir=$1
@@ -32,14 +33,11 @@ GUID_PREFIX="73646669-726D-6470-7500-0000"
 let block_cnt=$img_size/$BLOCK_SIZE
 part_align=$BLOCK_SIZE
 
-echo ">> Making GPT image $output_img size = $img_size bytes ($block_cnt blocks) from $input_dir"
-
 if [[ ! -d $input_dir ]]
 then
-	echo "Error: Input directory $input_dir not exists"
+	echo "Error: Input directory $input_dir not exists."
 	exit 0
 fi
-
 if [[ -f $output_img ]]
 then
 	echo "Error: Output file $output_img exists. Remove it first!"
@@ -52,14 +50,21 @@ fi
 cd $input_dir
 input_list=`find . -maxdepth 1 -type f | cut -d / -f 2`
 cd -
-echo ">> Input files under $input_dir"
-echo $input_list
+echo "============================================================"
+echo "Creating GPT Image..."
+echo "input directory: $input_dir"
+echo "input files: $input_list"
+echo "output file: $output_img"
+echo "image size: $img_size"
+echo "block count: $block_cnt"
 
 #
 # Create an empty image
 #
-dd if=/dev/zero of=./$output_img bs=$BLOCK_SIZE count=$block_cnt
-sgdisk --clear $output_img
+dd if=/dev/zero of=$output_img bs=$BLOCK_SIZE count=$block_cnt > /dev/null
+echo "------------------------------------------------------------"
+echo "Initializing GPT table..."
+sgdisk --clear $output_img > /dev/null
 
 part_num=1
 for f in $input_list
@@ -72,14 +77,25 @@ do
 	part_num_str=`printf %04x $part_num`
 	part_guid="$GUID_PREFIX""$part_num_str""$pad_size_str"
 	part_name=$f
-	echo ">> Adding partition $part_num $part_guid $part_name: $file_size + $pad_size(0x$pad_size_str) = $part_size bytes, $part_size_kbyte KB, UUID $part_guid"
-	sgdisk --new 0:0:+${part_size_kbyte}KB --change-name 0:$part_name --partition-guid 0:$part_guid $output_img
+	echo "------------------------------------------------------------"
+	echo "Add file GPT partition..."
+	echo "partition${part_num}: $part_num"
+	echo "UUID: $part_guid"
+	echo "file size: $file_size"
+	echo "pad size: $pad_size(0x$pad_size_str)"
+	echo "partition size: $part_size bytes, $part_size_kbyte KB"
+	sgdisk --new 0:0:+${part_size_kbyte}KB \
+		--change-name 0:$part_name \
+		--partition-guid 0:$part_guid $output_img > /dev/null
 	let part_num+=1
 done
 # One more partition for all remaining space
-sgdisk --new 0:0:-0 $output_img
+echo "------------------------------------------------------------"
+echo "Adding global GPT partition..."
+sgdisk --new 0:0:-0 $output_img > /dev/null
 
-echo ">> Initial partition information:"
+echo "------------------------------------------------------------"
+echo "GPT partition information:"
 parted $output_img unit B print
 
 #
@@ -93,11 +109,19 @@ do
 	part_start=`echo $part_info tr -s ' ' | cut -d ' ' -f 2 | cut -d 'B' -f 1`
 	let dd_seek=$part_start
 	let part_end=$dd_seek+$file_size
-	echo ">> Copy data into partiton $part_num seek $dd_seek start $part_start end $part_end size $file_size from file $f"
-	dd if=$input_dir/$f of=$output_img bs=1 count=$file_size seek=$dd_seek conv=notrunc
+	echo "------------------------------------------------------------"
+	echo "Copying file..."
+	echo "partition$part_num: $f"
+	echo "seek: $dd_seek"
+	echo "start: $part_start"
+	echo "end: $part_end"
+	echo "size: $file_size"
+	dd if=$input_dir/$f of=$output_img bs=1 count=$file_size \
+		seek=$dd_seek conv=notrunc > /dev/null
 	let part_num+=1
 done
 
-echo ">> Final partition information:"
+echo "------------------------------------------------------------"
+echo "Final partition information:"
 fdisk -l $output_img
-
+echo "============================================================"
