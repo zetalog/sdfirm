127388747a5775eb0fd175a175926d400f685a4a
Lv Zheng <zhenglv@hotmail.com>
2021-04-14 15:48:24 +0800

    sbi: Add SMP protection around sbi clock
    This patch adds SMP protection around SBI clocks.
    
    Signed-off-by: Lv Zheng <zhenglv@hotmail.com>

--- arch/riscv/sbi/sbi_clock.c
+++ arch/riscv/sbi/sbi_clock.c
@@ -1,21 +1,42 @@
 #include <target/sbi.h>
+#include <target/spinlock.h>
+
+DEFINE_SPINLOCK(sbi_clock_lock);
 
 unsigned long sbi_clock_get_freq(unsigned long clkid)
 {
-	return clk_get_frequency(clkid);
+	unsigned long freq;
+	irq_flags_t flags;
+
+	spin_lock_irqsave(&sbi_clock_lock, flags);
+	freq = clk_get_frequency(clkid);
+	spin_unlock_irqrestore(&sbi_clock_lock, flags);
+	return freq;
 }
 
 void sbi_clock_set_freq(unsigned long clkid, unsigned long freq)
 {
+	irq_flags_t flags;
+
+	spin_lock_irqsave(&sbi_clock_lock, flags);
 	clk_set_frequency(clkid, freq);
+	spin_unlock_irqrestore(&sbi_clock_lock, flags);
 }
 
 void sbi_clock_enable(unsigned long clkid)
 {
+	irq_flags_t flags;
+
+	spin_lock_irqsave(&sbi_clock_lock, flags);
 	clk_enable(clkid);
+	spin_unlock_irqrestore(&sbi_clock_lock, flags);
 }
 
 void sbi_clock_disable(unsigned long clkid)
 {
+	irq_flags_t flags;
+
+	spin_lock_irqsave(&sbi_clock_lock, flags);
 	clk_disable(clkid);
+	spin_unlock_irqrestore(&sbi_clock_lock, flags);
 }
