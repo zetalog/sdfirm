a7284172d4735531c7abc9540a41675f9d6029f9
Lv Zheng <zhenglv@hotmail.com>
2021-07-01 15:19:27 +0800

    duowen: Update PCIe ramp control settings
    This patch updates PCIe ramp control settings in the default
    configurations.
    
    Signed-off-by: Lv Zheng <zhenglv@hotmail.com>

--- include/target/gpio.h
+++ include/target/gpio.h
@@ -43,70 +43,7 @@
 #define __GPIO_H_INCLUDE__
 
 #include <target/generic.h>
-
-/*=========================================================================
- * GPIO special drive
- *=======================================================================*/
-#define GPIO_DRIVE_IN		0
-
-/*=========================================================================
- * GPIO mux configuration
- *=======================================================================*/
-#define GPIO_MUX_NONE		0
-
-/*=========================================================================
- * GPIO pad configuration
- *=======================================================================*/
-#define GPIO_PAD_PUSH_PULL	0x00
-#define GPIO_PAD_OPEN_DRAIN	0x01
-#define GPIO_PAD_PULL_UP	0x02
-#define GPIO_PAD_PULL_DOWN	0x04
-#define GPIO_PAD_ANALOG_IO	0x00
-#define GPIO_PAD_DIGITAL_IO	0x08
-#define GPIO_PAD_WEAK_PULL	0x00
-#define GPIO_PAD_MEDIUM_PULL	0x10
-#define GPIO_PAD_STRONG_PULL	0x20
-#define GPIO_PAD_SLEW_RATE	0x40
-#define GPIO_PAD_SCHMITT_TRIG	0x80
-/* Re-use schmitt trigger unless being complained */
-#define GPIO_PAD_KEEPER		0x80
-#define GPIO_PAD_PULL_MASK	\
-	(GPIO_PAD_PULL_UP | GPIO_PAD_PULL_DOWN)
-
-#define GPIO_PAD_PP		(GPIO_PAD_DIGITAL_IO | GPIO_PAD_PUSH_PULL)
-#define GPIO_PAD_OD		(GPIO_PAD_DIGITAL_IO | GPIO_PAD_OPEN_DRAIN)
-#define GPIO_PAD_KB		(GPIO_PAD_PP | GPIO_PAD_SLEW_RATE)
-#define GPIO_PAD_WU		(GPIO_PAD_PULL_UP | GPIO_PAD_WEAK_PULL)
-#define GPIO_PAD_WD		(GPIO_PAD_PULL_DOWN | GPIO_PAD_WEAK_PULL)
-#define GPIO_PAD_MU		(GPIO_PAD_PULL_UP | GPIO_PAD_MEDIUM_PULL)
-#define GPIO_PAD_MD		(GPIO_PAD_PULL_DOWN | GPIO_PAD_MEDIUM_PULL)
-#define GPIO_PAD_SU		(GPIO_PAD_PULL_UP | GPIO_PAD_STRONG_PULL)
-#define GPIO_PAD_SD		(GPIO_PAD_PULL_DOWN | GPIO_PAD_STRONG_PULL)
-
-/* push pull with weak pull up */
-#define GPIO_PAD_PP_WU		(GPIO_PAD_PP | GPIO_PAD_WU)
-/* push pull with weak pull down */
-#define GPIO_PAD_PP_WD		(GPIO_PAD_PP | GPIO_PAD_WD)
-/* push pull with medium pull up */
-#define GPIO_PAD_PP_MU		(GPIO_PAD_PP | GPIO_PAD_MU)
-/* push pull with medium pull down */
-#define GPIO_PAD_PP_MD		(GPIO_PAD_PP | GPIO_PAD_MD)
-/* push pull with strong pull up */
-#define GPIO_PAD_PP_SU		(GPIO_PAD_PP | GPIO_PAD_SU)
-/* push pull with medium pull down */
-#define GPIO_PAD_PP_SD		(GPIO_PAD_PP | GPIO_PAD_SD)
-/* open drain with weak pull up */
-#define GPIO_PAD_OD_WU		(GPIO_PAD_OD | GPIO_PAD_WU)
-/* open drain with weak pull down */
-#define GPIO_PAD_OD_WD		(GPIO_PAD_OD | GPIO_PAD_WD)
-/* open drain with medium pull up */
-#define GPIO_PAD_OD_MU		(GPIO_PAD_OD | GPIO_PAD_MU)
-/* open drain with medium pull down */
-#define GPIO_PAD_OD_MD		(GPIO_PAD_OD | GPIO_PAD_MD)
-/* open drain with strong pull up */
-#define GPIO_PAD_OD_SU		(GPIO_PAD_OD | GPIO_PAD_SU)
-/* open drain with strong pull down */
-#define GPIO_PAD_OD_SD		(GPIO_PAD_OD | GPIO_PAD_SD)
+#include <dt-bindings/pinctrl/sbi-pinctrl.h>
 
 /*=========================================================================
  * GPIO irq configuration
