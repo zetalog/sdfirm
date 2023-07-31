d9f2347bf2f7f9d44987ad13ae1a8ca3b3da5918
Zhen Yang <yangzhen.eric@spacemit.com>
2023-07-31 17:26:02 +0800

    modify cpu2006 run.sh

--- scripts/linux/cpu2006/run.sh
+++ scripts/linux/cpu2006/run.sh
@@ -5,10 +5,30 @@ for b in ${CPU2006_BENCHMARKS}; do
 			(
 			cd ${CPU2006_ROOT}${CPU2006_RUNBASE}
 			echo "Running ${s} ${b}..."
-			./${b}.sh
-			echo "cpu2006: ${CPU2006_ROOT}${CPU2006_RUNBASE}/speccmds.out begin"
-			cat ${CPU2006_ROOT}${CPU2006_RUNBASE}/speccmds.out
-			echo "cpu2006: ${CPU2006_ROOT}${CPU2006_RUNBASE}/speccmds.out end"
+			if [ "x$CPU2006_DUMP_ALL_FILES" = "xyes" ]; then
+				CPU2006_BEFORE=`ls`
+				./${b}.sh
+				CPU2006_AFTER=`ls`
+				for a in ${CPU2006_AFTER}; do
+					found=no
+					for b in ${CPU2006_BEFORE}; do
+						if [ "x$a" = "x$b" ]; then
+							found=yes
+							break
+						fi
+					done
+					if [ "x$found" = "xno" ]; then
+						echo "cpu2006: ${CPU2006_ROOT}${CPU2006_RUNBASE}/$a begin"
+						cat ${CPU2006_ROOT}${CPU2006_RUNBASE}/$a
+						echo "cpu2006: ${CPU2006_ROOT}${CPU2006_RUNBASE}/$a end"
+					fi
+				done
+			else
+				./${b}.sh
+				echo "cpu2006: ${CPU2006_ROOT}${CPU2006_RUNBASE}/speccmds.out begin"
+				cat ${CPU2006_ROOT}${CPU2006_RUNBASE}/speccmds.out
+				echo "cpu2006: ${CPU2006_ROOT}${CPU2006_RUNBASE}/speccmds.out end"
+			fi
 			)
 		fi
 	done
