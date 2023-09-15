84ad1da7344a60a139b8db07dc9d1dccceb77994
Chen Linfei <linfei.chen@spacemit.com>
2023-09-15 11:54:58 +0800

    fix PAD_BOOT_SEL_OFFSET PAD_DIE_ID_OFFSET

--- arch/riscv/include/asm/mach-k1matrix/sysreg.h
+++ arch/riscv/include/asm/mach-k1matrix/sysreg.h
@@ -95,14 +95,14 @@
 #define MESH_NID40_SYSCOACK	_BV(9)
 
 /* PAD_IN_STATUS */
-#define PAD_BOOT_SEL_OFFSET	0
+#define PAD_BOOT_SEL_OFFSET	1
 #define PAD_BOOT_SEL_MASK	REG_1BIT_MASK
 #define PAD_BOOT_SEL(value)	_GET_FV(PAD_BOOT_SEL, value)
-#define __PAD_BOOT_SEL		_BV(0)
-#define PAD_DIE_ID_OFFSET	1
+#define __PAD_BOOT_SEL		_BV(PAD_BOOT_SEL_OFFSET)
+#define PAD_DIE_ID_OFFSET	0
 #define PAD_DIE_ID_MASK		REG_1BIT_MASK
 #define PAD_DIE_ID(value)	_GET_FV(PAD_DIE_ID, value)
-#define __PAD_DIE_ID		_BV(1)
+#define __PAD_DIE_ID		_BV(PAD_DIE_ID_OFFSET)
 
 #define sysreg_soft_reset()		\
 	__raw_writel(CPU_GLOBAL_RESET, CPU_SW_RESET)
