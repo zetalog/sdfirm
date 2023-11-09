9e0b05d60e2f6b56efcdab5fbebb1edddaa7d4fd
Lv Zheng <zhenglv@hotmail.com>
2023-11-09 19:00:24 +0800

    arm64: tune dma sync APIs
    This patch tunes dma sync APIs.
    
    Signed-off-by: Lv Zheng <zhenglv@hotmail.com>

--- arch/arm64/include/asm/dma.h
+++ arch/arm64/include/asm/dma.h
@@ -3,10 +3,16 @@
 
 typedef phys_addr_t dma_addr_t;
 
-#define dma_hw_sync_dev(paddr, size, dir)	\
+#if 1
+#define dma_hw_sync_dev(dma, phys, size, dir)	dma_mb()
+#define dma_hw_sync_cpu(dma, phys, size, dir)	dma_mb()
+#define dma_hw_sync_cpu_all()			dma_mb()
+#else
+#define dma_hw_sync_dev(dma, paddr, size, dir)	\
 	__dma_map_area((void *)phys_to_virt(paddr), size, dir)
-#define dma_hw_sync_cpu(paddr, size, dir)	\
+#define dma_hw_sync_cpu(dma, paddr, size, dir)	\
 	__dma_unmap_area((void *)phys_to_virt(paddr), size, dir)
 #define dma_hw_sync_cpu_all()			do { } while (0)
+#endif
 
 #endif /* __DMA_ARM64_H_INCLUDE__ */
