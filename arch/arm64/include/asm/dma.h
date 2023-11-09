a867653b4de128d3142f0d59e53dbc89dff5e9c6
Lv Zheng <zhenglv@hotmail.com>
2023-11-09 18:55:31 +0800

    arm: Fix missing dma_addr_t
    This patch fixes missing dma_addr_t.
    
    Signed-off-by: Lv Zheng <zhenglv@hotmail.com>

--- arch/arm64/include/asm/dma.h
+++ arch/arm64/include/asm/dma.h
@@ -1,6 +1,8 @@
 #ifndef __DMA_ARM64_H_INCLUDE__
 #define __DMA_ARM64_H_INCLUDE__
 
+typedef phys_addr_t dma_addr_t;
+
 #define dma_hw_sync_dev(paddr, size, dir)	\
 	__dma_map_area((void *)phys_to_virt(paddr), size, dir)
 #define dma_hw_sync_cpu(paddr, size, dir)	\
