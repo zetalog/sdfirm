8e657109296d4aa18a127eea2c74bc256b7acbd7
Lv Zheng <zhenglv@hotmail.com>
2023-11-26 09:14:12 +0800

    iommu_armv8: Fix PGD zeroing
    This patch fixes PGD zeroing.
    
    Signed-off-by: Lv Zheng <zhenglv@hotmail.com>

--- drivers/iommu/iommu_armv8.c
+++ drivers/iommu/iommu_armv8.c
@@ -203,6 +203,8 @@ static void *__arm_lpae_alloc_pages(size_t size, struct io_pgtable_cfg *cfg)
 		}
 	}
 #endif
+	if (pages)
+		memory_set((caddr_t)pages, 0, size);
 	con_log("lpae: PGD=%016llx, SIZE=%016llx\n",
 		(unsigned long long)p, (unsigned long long)size);
 	return pages;
