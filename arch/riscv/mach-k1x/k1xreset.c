664d7d4548bc7536eda9cbab3d7ae2278d9c5c1e
Zhen Yang <yangzhen.eric@spacemit.com>
2023-12-05 19:50:36 +0800

    k1x: add support for 1x4

--- arch/riscv/mach-k1x/k1xreset.c
+++ arch/riscv/mach-k1x/k1xreset.c
@@ -4,16 +4,16 @@
 
 void k1x_cpu_reset(void)
 {
-        __raw_writel(0x40000000, (uint32_t *)0xD4282DB0);
-        __raw_writel((0x40000000 >> 32) & 0xffffffff, (uint32_t *)0xD4282DB4);
-        __raw_writel(1 << 1, 0xD428292C);
-        __raw_writel(1 << 2, 0xD428292C);
-        __raw_writel(1 << 3, 0xD428292C);
-    
-        // __raw_writel(0x40000000, (uint32_t *)(0xD4282C00 + 0x2B0));
-        // __raw_writel((0x40000000 >> 32) & 0xffffffff, (uint32_t *)(0xD4282C00 + 0x2B4));
-        // __raw_writel(1 << 4, 0xD428292C);
-        // __raw_writel(1 << 5, 0xD428292C);
-        //__raw_writel(1 << 6, 0xD428292C);
-        //__raw_writel(1 << 7, 0xD428292C);
+	__raw_writel(0x40000000, (uint32_t *)0xD4282DB0);
+	__raw_writel((0x40000000 >> 32) & 0xffffffff, (uint32_t *)0xD4282DB4);
+	__raw_writel(1 << 1, 0xD428292C);
+	__raw_writel(1 << 2, 0xD428292C);
+	__raw_writel(1 << 3, 0xD428292C);
+
+	// __raw_writel(0x40000000, (uint32_t *)(0xD4282C00 + 0x2B0));
+	// __raw_writel((0x40000000 >> 32) & 0xffffffff, (uint32_t *)(0xD4282C00 + 0x2B4));
+	// __raw_writel(1 << 4, 0xD428292C);
+	// __raw_writel(1 << 5, 0xD428292C);
+	//__raw_writel(1 << 6, 0xD428292C);
+	//__raw_writel(1 << 7, 0xD428292C);
 }
