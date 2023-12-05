0109b2ff1ee01af3a32516cb96b0b3ef10972c1c
Zhen Yang <yangzhen.eric@spacemit.com>
2023-12-05 20:38:43 +0800

    LPC: driver filled

---
+++ drivers/lpc/spacemit_lpc.c
@@ -0,0 +1,127 @@
+#include <spacemit_lpc.h>
+
+uint8_t lpc_get_config(void)
+{
+	return spacemit_lpc_get_config;
+}
+
+uint8_t lpc_set_config(uint8_t byte)
+{
+	return spacemit_lpc_set_config(byte);
+}
+
+uint8_t lpc_read_start(void)
+{
+	return spacemit_lpc_read_start;
+}
+
+uint8_t lpc_write_start(void)
+{
+	return spacemit_lpc_write_start;
+}
+
+uint8_t lpc_get_status(void)
+{
+	return spacemit_lpc_get_status;
+}
+
+uint8_t lpc_get_int_mask(void)
+{
+	return spacemit_lpc_get_int_mask;
+}
+
+uint8_t lpc_set_int_mask(uint8_t byte)
+{
+	return spacemit_lpc_set_int_mask(byte);
+}
+
+uint8_t lpc_get_int_status(void)
+{
+	return spacemit_get_int_status;
+}
+
+uint8_t lpc_get_int_raw_status(void)
+{
+	return spacemit_get_int_raw_status;
+}
+
+uint8_t lpc_int_clear(uint8_t byte)
+{
+	return spacemit_lpc_int_clear(byte);
+}
+
+uint8_t lpc_get_wait_count(void)
+{
+	return spacemit_lpc_get_wait_count;
+}
+
+uint8_t lpc_set_wait_count(uint8_t byte)
+{
+	return spacemit_lpc_set_wait_count(byte);
+}
+
+uint8_t lpc_get_addr(void)
+{
+	return spacemit_lpc_get_addr;
+}
+
+uint8_t lpc_set_addr(uint8_t byte)
+{
+	return spacemit_lpc_set_addr(byte);
+}
+
+uint8_t lpc_write_data(uint8_t byte)
+{
+	return spacemit_lpc_write_data(byte);
+}
+
+uint8_t lpc_read_data(void)
+{
+	return spacemit_lpc_read_data;
+}
+
+uint8_t lpc_get_debug(void)
+{
+	return spacemit_lpc_get_debug;
+}
+
+uint8_t lpc_get_serirq_cfg(void)
+{
+	return spacemit_lpc_get_serirq_cfg;
+}
+
+uint8_t lpc_set_serirq_cfg(uint8_t byte)
+{
+	return spacemit_lpc_set_serirq_cfg(byte);
+}
+
+uint8_t lpc_set_serirq_op(uint8_t byte)
+{
+	return spacemit_lpc_set_serirq_op(byte);
+}
+
+uint8_t lpc_get_serirq_slot_mask(void)
+{
+	return spacemit_lpc_get_serirq_slot_mask;
+}
+
+uint8_t lpc_set_serirq_slot_mask(uint8_t byte)
+{
+	return spacemit_lpc_set_serirq_slot_mask(byte);
+}
+
+uint8_t lpc_get_serirq_slot_irq(void)
+{
+	return spacemit_lpc_get_serirq_slot_irq;
+}
+
+uint8_t lpc_set_serirq_slot_clr(uint8_t byte)
+{
+	return spacemit_lpc_set_serirq_slot_clr(byte);
+}
+
+uint8_t lpc_get_serirq_slot_debug(void)
+{
+	return spacemit_lpc_get_serirq_slot_debug;
+}
+
