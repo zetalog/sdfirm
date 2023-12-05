621a8c9b3271aab542d2e9c2c40756d741a99e44
Chen Linfei <linfei.chen@spacemit.com>
2023-12-05 09:54:13 +0800

    add PCIE_LINK_MODE CONFIG

--- arch/riscv/include/asm/mach-k1matrix/pci.h
+++ arch/riscv/include/asm/mach-k1matrix/pci.h
@@ -1,6 +1,10 @@
 #ifndef __PCI_K1MATRIX_H_INCLUDE__
 #define __PCI_K1MATRIX_H_INCLUDE__
 
+#include <driver/pcie_designware.h>
+
+#define PCIE_LINK_MODE PORT_LINK_MODE_4_LANES
+
 #ifdef CONFIG_K1MATRIX_PCIE
 void pcie_ccix_linkup(void);
 #else
