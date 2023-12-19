c5ebf4aaaf36aef022e8659881bb343e61699ac8
Zhen Yang <yangzhen.eric@spacemit.com>
2023-12-19 16:42:40 +0800

    k1x: modified for board

--- include/sbi/sbi_bitops.h
+++ include/sbi/sbi_bitops.h
@@ -144,36 +144,6 @@ unsigned long find_next_zero_bit(const unsigned long *addr,
 				 unsigned long offset);
 
 /**
- * __set_bit - Set a bit in memory
- * @nr: the bit to set
- * @addr: the address to start counting from
- *
- * This function is non-atomic and may be reordered.
- */
-//static inline void __set_bit(int nr, volatile unsigned long *addr)
-//{
-//	unsigned long mask = BIT_MASK(nr);
-//	unsigned long *p = ((unsigned long *)addr) + BIT_WORD(nr);
-//
-//	*p |= mask;
-//}
-
-/**
- * __clear_bit - Clear a bit in memory
- * @nr: the bit to clear
- * @addr: the address to start counting from
- *
- * This function is non-atomic and may be reordered.
- */
-//static inline void __clear_bit(int nr, volatile unsigned long *addr)
-//{
-//	unsigned long mask = BIT_MASK(nr);
-//	unsigned long *p = ((unsigned long *)addr) + BIT_WORD(nr);
-//
-//	*p &= ~mask;
-//}
-
-/**
  * __change_bit - Toggle a bit in memory
  * @nr: the bit to change
  * @addr: the address to start counting from
@@ -188,39 +158,6 @@ static inline void __change_bit(int nr, volatile unsigned long *addr)
 	*p ^= mask;
 }
 
-/**
- * __test_and_set_bit - Set a bit and return its old value
- * @nr: Bit to set
- * @addr: Address to count from
- *
- * This operation is non-atomic and can be reordered.
- */
-//static inline int __test_and_set_bit(int nr, volatile unsigned long *addr)
-//{
-//	unsigned long mask = BIT_MASK(nr);
-//	unsigned long *p = ((unsigned long *)addr) + BIT_WORD(nr);
-//	unsigned long old = *p;
-//
-//	*p = old | mask;
-//	return (old & mask) != 0;
-//}
-
-/**
- * __test_and_clear_bit - Clear a bit and return its old value
- * @nr: Bit to clear
- * @addr: Address to count from
- *
- * This operation is non-atomic and can be reordered.
- */
-static inline int __test_and_clear_bit(int nr, volatile unsigned long *addr)
-{
-	unsigned long mask = BIT_MASK(nr);
-	unsigned long *p = ((unsigned long *)addr) + BIT_WORD(nr);
-	unsigned long old = *p;
-
-	*p = old & ~mask;
-	return (old & mask) != 0;
-}
 
 /**
  * __test_bit - Determine whether a bit is set
