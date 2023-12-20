5a257ce37ba2e3d2995ff2ef5a0d3d5af04a26b6
Zhen Yang <yangzhen.eric@spacemit.com>
2023-12-20 14:43:10 +0800

    lpc: finished driver

---
+++ arch/riscv/include/asm/mach-k1matrix/lpc.h
@@ -0,0 +1,8 @@
+#ifndef __LPC_K1MATRIX_H_INCLUDE__
+#define __LPC_K1MATRIX_H_INCLUDE__
+
+#ifdef CONFIG_K1MATRIX_LPC
+#include <driver/spacemit_lpc.h>
+#endif
+
+#endif /* __LPC_K1MATRIX_H_INCLUDE__ */
