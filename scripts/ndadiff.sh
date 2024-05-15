06cd7a6ab3fd02c9b196e06f2af0676ed26de81d
Lv Zheng <zhenglv@hotmail.com>
2024-05-15 17:21:12 +0800

    Add update option
    This patch adds update option to ndadiff.sh
    
    Signed-off-by: Lv Zheng <zhenglv@hotmail.com>

--- scripts/ndadiff.sh
+++ scripts/ndadiff.sh
@@ -37,4 +37,29 @@ walk_source()
 	fi
 }
 
+usage()
+{
+	echo "Usage:"
+	echo "`basename $0` [-u]"
+	echo "Where:"
+	echo " -u:          update $SDFIRM_ROOT with $SDFIRM_NDA"
+	exit $1
+}
+
+fatal_usage()
+{
+	echo $1
+	usage 1
+}
+
+while getopts "u" opt
+do
+	case $opt in
+	u) UPDATE=yes;;
+	?) echo "Invalid argument $opt"
+	   fatal_usage;;
+	esac
+done
+shift $(($OPTIND - 1))
+
 walk_source $SDFIRM_ROOT $SDFIRM_NDA
