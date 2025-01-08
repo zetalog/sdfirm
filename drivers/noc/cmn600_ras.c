7cad72833d3742330363c889ac6a6c048f860042
Zhen Yang <yangzhen.eric@spacemit.com>
2025-01-08 17:05:20 +0800

    update ras

--- drivers/noc/cmn600_ras.c
+++ drivers/noc/cmn600_ras.c
@@ -33,7 +33,7 @@ void cmn600_ras_config(caddr_t base)
 void cmn600_ras_report(caddr_t base)
 {
 	uint64_t status = __raw_readq(CMN_errstatus(base));
-	//printf("ras_report: %llx\n", status);
+	printf("ras_report: %llx\n", status);
 	if (status & CMN_errstatus_AV)
 		con_log("cmn_ras: AV Error: addr=%08llx\n", __raw_readq(CMN_erraddr(base)));
 	if (status & CMN_errstatus_V)
@@ -49,7 +49,7 @@ void cmn600_ras_report(caddr_t base)
 	if (status & CMN_errstatus_DE)
 		con_log("cmn_ras: DE Error\n");
 
-	__raw_writeq(status, CMN_errstatus(base));
+	cmn_writeq(status, CMN_errstatus(base), "CMN_errstatus", -1);
 }
 
 caddr_t CMN_ras_nid(uint8_t errg, uint8_t bit)
@@ -210,45 +210,44 @@ void cmn600_ras_init(void)
 	cmn600_ras_poll_init();
 }
 
-void cmn600_ras_hnf_err_inj(uint64_t id, uint64_t srcid, uint64_t lpid)
+void cmn600_ras_hnf_err_inj(cmn_nid_t id, cmn_nid_t srcid, uint16_t lpid)
 {
-	printf("hnf_errinj: hnf_id=%d, hnf_base=0x%lx, srcid=0x%llx, lpid=0x%llx\n", 
-		cmn_hnf_ids[id], cmn_bases[cmn_hnf_ids[id]], srcid, lpid);
+	printf("hnf_errinj: hnf_id=%d, hnf_base=0x%lx, srcid=0x%x, lpid=0x%x\n", 
+		id, cmn600_hnf_base(id), srcid, lpid);
 	cmn_writeq_mask(CMN_hnf_err_inj_srcid(srcid), 
 			CMN_hnf_err_inj_srcid(CMN_hnf_err_inj_srcid_MASK), 
-			CMN_hnf_err_inj(cmn_bases[cmn_hnf_ids[id]]), 
+			CMN_hnf_err_inj(cmn600_hnf_base(id)), 
 			"CMN_hnf_err_inj", -1);
 	cmn_writeq_mask(CMN_hnf_err_inj_lpid(lpid), 
 			CMN_hnf_err_inj_lpid(CMN_hnf_err_inj_lpid_MASK), 
-			CMN_hnf_err_inj(cmn_bases[cmn_hnf_ids[id]]), 
+			CMN_hnf_err_inj(cmn600_hnf_base(id)), 
 			"CMN_hnf_err_inj", -1);
 	cmn_setq(CMN_hnf_err_inj_en, 
-		 CMN_hnf_err_inj(cmn_bases[cmn_hnf_ids[id]]), 
+		 CMN_hnf_err_inj(cmn600_hnf_base(id)), 
 		 "CMN_hnf_err_inj", -1);
 }	
 
-void cmn600_ras_hnf_par_err_inj(uint64_t id, uint64_t lane)
+void cmn600_ras_hnf_par_err_inj(cmn_nid_t id, uint16_t lane)
 {
-	printf("hnf_par_errinj: hnf_id=%d, hnf_base=0x%lx, lane=0x%llx\n", 
-		cmn_hnf_ids[id], cmn_bases[cmn_hnf_ids[id]], lane);
+	printf("hnf_par_errinj: hnf_id=%d, hnf_base=0x%lx, lane=0x%x\n", 
+		id, cmn600_hnf_base(id), lane);
 	cmn_writeq(lane, 
-		   CMN_hnf_byte_par_err_inj(cmn_bases[cmn_hnf_ids[id]]), 
+		   CMN_hnf_byte_par_err_inj(cmn600_hnf_base(id)), 
 		   "CMN_hnf_byte_par_err_inj", -1);
 }
 
-void cmn600_ras_mxp_par_err_inj(uint64_t id, uint64_t port, uint64_t lane)
+void cmn600_ras_mxp_par_err_inj(cmn_nid_t id, uint16_t port, uint16_t lane)
 {
-	printf("mxp_par_errinj: xp_id=%d, xp_base=0x%lx, port=%lld, lane=0x%llx\n", 
-		cmn_xp_ids[id], cmn_bases[cmn_xp_ids[id]], port, lane);
+	printf("mxp_par_errinj: xp_id=%d, xp_base=0x%lx, port=%d, lane=0x%x\n", 
+		id, cmn_bases[cmn600_nid2xp(id)], port, lane);
 	cmn_writeq(lane, 
-		   CMN_mxp_byte_par_err_inj(cmn_bases[cmn_xp_ids[id]], port), 
+		   CMN_mxp_byte_par_err_inj(cmn600_nid2xp(id), port), 
 		   "CMN_mxp_byte_par_err_inj", -1);
 }
 
 static int do_cmn_ras(int argc, char *argv[])
 {
-	cmn_nid_t nid;
-	caddr_t base;
+	uint32_t i;
 
 	if (argc < 2)
 		return -EINVAL;
@@ -259,82 +258,65 @@ static int do_cmn_ras(int argc, char *argv[])
 	if (strcmp(argv[1], "einj") == 0) {
 		if (argc < 6)
 			return -EINVAL;
-		if (strcmp(argv[2], "par") == 0) {
-			if (strcmp(argv[3], "mxp") == 0) {
-				uint16_t cpu = (uint16_t)strtoull(argv[4], 0, 0);
-				uint64_t lane = (uint64_t)strtoull(argv[5], 0, 0);
-				uint16_t clst;
-				if (cpu > MAX_CPU_NUM) {
-					printf("Invalid cpu %d\n", cpu);
-				}
-				clst = cpu / CPUS_PER_CLUSTER;
-				nid = cmn_rnf_table[clst];
-				if (lane >= (CMN600_WLEN / 8)) {
-					printf("Invalid lane %llx >= %d\n", lane, (CMN600_WLEN / 8));
-					return -EINVAL;
-				}
-				cmn600_ras_mxp_par_err_inj(cmn600_nid2xp(nid),
-								CMN_PID(nid),
-								lane);
-				return 0;
+		if (strcmp(argv[2], "cpu") == 0) {
+			uint16_t hnf = (uint16_t)strtoull(argv[3], 0, 0);
+			uint16_t cpu = (uint16_t)strtoull(argv[4], 0, 0);
+			uint16_t lane = (uint16_t)strtoull(argv[5], 0, 0);
+			uint16_t clst, lpid, rnf;
+			for (i = 0; i < CMN_MAX_HNF_COUNT; i++) {
+				if (hnf == cmn_hnf_table[i])
+					break;
 			}
-			if (strcmp(argv[3], "hnf") == 0) {
-				uint16_t hnf = (uint16_t)strtoull(argv[4], 0, 0);
-				uint64_t lane = (uint64_t)strtoull(argv[5], 0, 0);
-				if (hnf >= cmn_hnf_count) {
-					printf("Invalid hnf %d\n", hnf);
-					return -EINVAL;
-				}
-				if (lane >= (CMN600_WLEN / 8)) {
-					printf("Invalid lane %llx >= %d\n", lane, (CMN600_WLEN / 8));
-					return -EINVAL;
-				}
-				cmn600_ras_hnf_par_err_inj(hnf, lane);
-				return 0;
+			if (i == CMN_MAX_HNF_COUNT) {
+				printf("Invalid hnf %d\n", hnf);
+				return -EINVAL;
 			}
+			if (cpu > MAX_CPU_NUM) {
+				printf("Invalid cpu %d\n", cpu);
+				return -EINVAL;
+			}
+			if (lane >= (CMN600_WLEN / 8)) {
+				printf("Invalid lane %x >= %d\n", lane, (CMN600_WLEN / 8));
+				return -EINVAL;
+			}
+			clst = cpu / CPUS_PER_CLUSTER;
+			lpid = cpu % CPUS_PER_CLUSTER;
+			rnf = cmn_rnf_table[clst];
+			cmn600_ras_hnf_err_inj(hnf, rnf, lpid);
+			cmn600_ras_hnf_par_err_inj(hnf, lane);
+			return 0;
 		}
-		if (strcmp(argv[2], "ecc") == 0) {
-			if (strcmp(argv[3], "cpu") == 0) {
-				uint16_t hnf = (uint16_t)strtoull(argv[4], 0, 0);
-				uint16_t cpu = (uint16_t)strtoull(argv[5], 0, 0);
-				uint16_t clst, lpid;
-				if (cpu > MAX_CPU_NUM) {
-					printf("Invalid cpu %d\n", cpu);
-					return -EINVAL;
-				}
-				if (hnf >= cmn_hnf_count) {
-					printf("Invalid hnf %d\n", hnf);
-					return -EINVAL;
-				}
-				clst = cpu / CPUS_PER_CLUSTER;
-				lpid = cpu % CPUS_PER_CLUSTER;
-				nid = cmn_rnf_table[clst];
-				cmn600_ras_hnf_err_inj(hnf, nid, lpid);
-				return 0;
+		if (strcmp(argv[2], "dma") == 0) {
+			uint16_t hnf = (uint16_t)strtoull(argv[3], 0, 0);
+			uint16_t rni = (uint16_t)strtoull(argv[4], 0, 0);
+			uint16_t lane = (uint16_t)strtoull(argv[5], 0, 0);
+			uint16_t axid = 0, portid = 0, lpid = 0;
+			caddr_t base = 0;
+			for (i = 0; i < CMN_MAX_HNF_COUNT; i++) {
+				if (hnf == cmn_hnf_table[i])
+					break;
 			}
-			if (strcmp(argv[3], "dma") == 0) {
-				uint16_t hnf = (uint16_t)strtoull(argv[4], 0, 0);
-				uint16_t rni = (uint16_t)strtoull(argv[5], 0, 0);
-				uint16_t axid = 0, portid = 0;
-				uint16_t lpid = 0;
-				if (hnf >= cmn_hnf_count) {
-					printf("Invalid hnf %d\n", hnf);
-					return -EINVAL;
-				}
-				if (rni >= cmn_rni_count) {
-					printf("Invalid rni %d\n", rni);
-					return -EINVAL;
-				}
-				base = cmn_bases[cmn_rni_ids[rni]];
-				nid = cmn_node_id(base);
-				if (argc > 7) {
-					axid = (uint16_t)strtoull(argv[6], 0, 0);
-					portid = (uint16_t)strtoull(argv[7], 0, 0);
-					lpid = (__raw_readq(CMN_rni_s_port_control(base, portid)) & axid & 0x1) | (portid << 1);
-				}
-				cmn600_ras_hnf_err_inj(hnf, nid, lpid);
-				return 0;
+			if (i == CMN_MAX_HNF_COUNT) {
+				printf("Invalid hnf %d\n", hnf);
+				return -EINVAL;
 			}
+			if (!cmn600_rnsam_is_rni(rni)) {
+				printf("Invalid rni %d\n", rni);
+				return -EINVAL;
+			}
+			if (lane >= (CMN600_WLEN / 8)) {
+				printf("Invalid lane %x >= %d\n", lane, (CMN600_WLEN / 8));
+				return -EINVAL;
+			}
+			if (argc > 7) {
+				axid = (uint16_t)strtoull(argv[6], 0, 0);
+				portid = (uint16_t)strtoull(argv[7], 0, 0);
+				base = cmn600_rni_base(rni);
+				lpid = (__raw_readq(CMN_rni_s_port_control(base, portid)) & axid & 0x1) | (portid << 1);
+			}
+			cmn600_ras_hnf_err_inj(hnf, rni, lpid);
+			cmn600_ras_hnf_par_err_inj(hnf, lane);
+			return 0;
 		}
 		return -EINVAL;
 	}
@@ -343,12 +325,8 @@ static int do_cmn_ras(int argc, char *argv[])
 
 DEFINE_COMMAND(cmn_ras, do_cmn_ras, "SpacemiT CMN RAS Debug commands",
 	"cmn_ras init\n"
-	"cmn_ras einj par mxp <cpu> <lane>\n"
-	"  - inject parity error into XP from CPU"
-	"cmn_ras einj par hnf <hnf> <lane>\n"
-	"  - inject parity error for SLC hit in HN-F\n"
-	"cmn_ras einj ecc cpu <hnf> <cpu>\n"
+	"cmn_ras einj cpu <hnf> <cpu> <lane>\n"
 	"  - inject double-bit ECC error for SLC hit from given CPU\n"
-	"cmn_ras einj ecc dma <hnf> <rni> [axid] [portid]\n"
+	"cmn_ras einj dma <hnf> <rni> <lane> [axid] [portid]\n"
 	"  - inject double-bit ECC error for SLC hit from give DMA\n"
 );
