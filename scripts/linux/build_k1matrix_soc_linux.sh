a3be2568c131e1410596fc9f8e8dc737ac47c44b
Lv Zheng <zhenglv@hotmail.com>
2025-05-15 09:21:14 +0800

    linux: Add build option of OCM and NUMA
    This patch enables OCM/NUMA configurable.
    
    Signed-off-by: Lv Zheng <zhenglv@hotmail.com>

--- scripts/linux/build_k1matrix_soc_linux.sh
+++ scripts/linux/build_k1matrix_soc_linux.sh
@@ -19,6 +19,8 @@ BUILD_SMP=no
 BUILD_NET=no
 BUILD_STO=no
 BUILD_KVM=no
+BUILD_OCM=yes
+BUILD_NUMA=no
 
 # If you only want to re-build kernel drivers, you can use no to omit
 # rebuilding whole busybox/linux
