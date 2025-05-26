59683e9f9867b0fcb146f36b3753d4137c2cb66e
Zhen Yang <yangzhen.eric@spacemit.com>
2025-05-27 00:08:44 +0800

    add cmn600 trace test cmds

--- drivers/pmu/cmn600_trace.c
+++ drivers/pmu/cmn600_trace.c
@@ -1,21 +1,88 @@
 #include <target/noc.h>
+#include <target/perf.h>
 #include <target/irq.h>
 #include <target/cmdline.h>
 #include <target/console.h>
 #include <target/arch.h>
 
-uint8_t cmn_trace_cfg_watchpoint(caddr_t base, uint8_t wp, uint64_t addr, uint64_t mask)
+static int cmn_trace_wp_config(caddr_t base, int wp, int dev_sel,
+				int chn_sel, int pkt_type, int pkt_gen,
+				int combine, int cc_en, int ctrigger_en,
+				int dbgtrigger_en)
 {
-	if (wp >= CMN_TRACE_WP_COUNT)
-		return -EINVAL;
-	cmn_writeq(addr, CMN_trace_wp_addr(base, wp), "CMN_trace_wp_addr", -1);
-	cmn_writeq(mask, CMN_trace_wp_mask(base, wp), "CMN_trace_wp_mask", -1);
+	uint64_t val = 0;
+
+	val |= dtm_wp_cfg_dev_sel;
+	val |= dtm_wp_cfg_chn_sel(chn_sel);
+	if (dev_sel)
+		val |= dtm_wp_cfg_dev_sel;
+	if (combine)
+		val |= dtm_wp_cfg_combine;
+	if (pkt_gen)
+		val |= dtm_wp_cfg_pkt_gen;
+	val |= dtm_wp_cfg_pkt_type(pkt_type);
+	if (cc_en)
+		val |= dtm_wp_cfg_cc_en;
+	if (ctrigger_en)
+		val |= dtm_wp_cfg_ctrig_en;
+	if (dbgtrigger_en)
+		val |= dtm_wp_cfg_dbgtrig_en;
+
+	__raw_writeq(val, CMN_dtm_wp_config(base, wp));
+	return 0;
+}
+
+static int cmn_trace_wp_val_mask(caddr_t base, int wp, uint64_t val, uint64_t mask)
+{
+	__raw_writeq(val, CMN_dtm_wp_val(base, wp));
+	__raw_writeq(mask, CMN_dtm_wp_mask(base, wp));
+	return 0;
+}
+
+static int cmn_trace_wp_enable(caddr_t base, int wp, int enable)
+{
+	uint64_t val = __raw_readq(CMN_dtm_wp_config(base, wp));
+	if (enable)
+		val |= dtm_control_en;
+	else
+		val &= ~dtm_control_en;
+	__raw_writeq(val, CMN_dtm_wp_config(base, wp));
+	return 0;
+}
+
+static int cmn_trace_wp_setup(caddr_t base, int wp, int dev_sel,
+				  int chn_sel, int pkt_type, int pkt_gen,
+				  int combine, int cc_en, int ctrigger_en,
+				  int dbgtrigger_en, uint64_t val, uint64_t mask)
+{
+	cmn_trace_wp_config(base, wp, dev_sel, chn_sel, pkt_type,
+			    pkt_gen, combine, cc_en, ctrigger_en,
+			    dbgtrigger_en);
+	cmn_trace_wp_val_mask(base, wp, val, mask);
+	return cmn_trace_wp_enable(base, wp, 1);
+}
+
+static int cmn_trace_dt_setup(caddr_t base, int dbgtrigger_en,
+			      int atbtrigger_en, int wait_for_trigger)
+{
+	uint64_t val = 0;
+
+	if (dbgtrigger_en)
+		val |= dt_dtc_ctl_dbgtrigger_en;
+	if (atbtrigger_en)
+		val |= dt_dtc_ctl_atbtrigger_en;
+	if (wait_for_trigger)
+		val |= dt_dtc_ctl_wait_for_trigger;
+	val |= dt_dtc_ctl_en;
+
+	__raw_setq(dt_trace_control_cc_enable, CMN_dt_trace_control(base));
+	__raw_writeq(val, CMN_dt_ctl(base));
 	return 0;
 }
 
 static int do_cmn_trace(int argc, char *argv[])
 {
-	int i;
+	int i, id;
 	if (argc < 2)
 		return -EINVAL;
 	if (strcmp(argv[1], "list") == 0) {
@@ -26,10 +93,51 @@ static int do_cmn_trace(int argc, char *argv[])
 		}
 		return 0;
 	}
+	if (strcmp(argv[1], "wp_setup") == 0) {
+		if (argc < 14)
+			return -EINVAL;
+		id = atoi(argv[2]);
+		if (id < 0 || id >= cmn_dtc_count)
+			return -EINVAL;
+		caddr_t base = cmn_bases[cmn_dtc_ids[id]];
+		int wp = atoi(argv[3]);
+		int dev_sel = atoi(argv[4]);
+		int chn_sel = atoi(argv[5]);
+		int pkt_type = atoi(argv[6]);
+		int pkt_gen = atoi(argv[7]);
+		int combine = atoi(argv[8]);
+		int cc_en = atoi(argv[9]);
+		int ctrigger_en = atoi(argv[10]);
+		int dbgtrigger_en = atoi(argv[11]);
+		uint64_t val = strtoull(argv[12], NULL, 0);
+		uint64_t mask = strtoull(argv[13], NULL, 0);
+		return cmn_trace_wp_setup(base, wp, dev_sel, chn_sel,
+					  pkt_type, pkt_gen, combine,
+					  cc_en, ctrigger_en,
+					  dbgtrigger_en, val, mask);
+	}
+	if (strcmp(argv[1], "dt_setup") == 0) {
+		if (argc < 5)
+			return -EINVAL;
+		id = atoi(argv[2]);
+		if (id < 0 || id >= cmn_dtc_count)
+			return -EINVAL;
+		caddr_t base = cmn_bases[cmn_dtc_ids[id]];
+		int dbgtrigger_en = atoi(argv[3]);
+		int atbtrigger_en = atoi(argv[4]);
+		int wait_for_trigger = argc > 5 ? atoi(argv[5]) : 0;
+		return cmn_trace_dt_setup(base, dbgtrigger_en,
+					  atbtrigger_en, wait_for_trigger);
+	}
+
+	return -EINVAL;
 }
 
 DEFINE_COMMAND(cmn_trace, do_cmn_trace, "SpacemiT CMN Debug Trace configuration commands",
 	"cmn_trace list\n"
 	"  - list all cmn dt registers\n"
-
+	"cmn_trace wp_setup <id> <wp> <dev_sel> <chn_sel> <pkt_type> <pkt_gen> <combine> <cc_en> <ctrigger_en> <dbgtrigger_en> <val> <mask>\n"
+	"  - setup a watchpoint with the given parameters\n"
+	"cmn_trace dt_setup <id> <dbgtrigger_en> <atbtrigger_en> <wait_for_trigger>\n"
+	"  - setup the dt control with the given parameters\n"
 );
--- include/driver/cmn600_trace.h
+++ include/driver/cmn600_trace.h
@@ -3,48 +3,84 @@
 
 #include <target/noc.h>
 
-/* dtm_wp_REQ */
-#define dtm_wp_REQ_SRCID_OFFSET		0
-#define dtm_wp_REQ_SRCID_MASK		REG_11BIT_MASK
-#define dtm_wp_REQ_SRCID(value)		_SET_FV_ULL(dtm_wp_REQ_SRCID, value)
-#define dtm_wp_REQ_TGTID_OFFSET		0
-#define dtm_wp_REQ_TGTID_MASK		REG_11BIT_MASK
-#define dtm_wp_REQ_TGTID(value)		_SET_FV_ULL(dtm_wp_REQ_TGTID, value)
-#define dtm_wp_REQ_OPCODE_OFFSET	31
-#define dtm_wp_REQ_OPCODE_MASK		REG_6BIT_MASK
-#define dtm_wp_REQ_OPCODE(value)	_SET_FV_ULL(dtm_wp_REQ_OPCODE, value)
+/* dtm_control */
+#define dtm_control_en				_BV(0)
+#define dtm_control_trace_tag_en		_BV(1)
 
-/* dtm_wp_RSP */
-#define dtm_wp_RSP_SRCID_OFFSET		4
-#define dtm_wp_RSP_SRCID_MASK		REG_11BIT_MASK
-#define dtm_wp_RSP_SRCID(value)		_SET_FV_ULL(dtm_wp_RSP_SRCID, value)
-#define dtm_wp_RSP_TGTID_OFFSET		4
-#define dtm_wp_RSP_TGTID_MASK		REG_11BIT_MASK
-#define dtm_wp_RSP_TGTID(value)		_SET_FV_ULL(dtm_wp_RSP_TGTID, value)
-#define dtm_wp_RSP_OPCODE_OFFSET	15
-#define dtm_wp_RSP_OPCODE_MASK		REG_4BIT_MASK
-#define dtm_wp_RSP_OPCODE(value)	_SET_FV_ULL(dtm_wp_RSP_OPCODE, value)
+/* dtm_wp_cfg */
+#define dtm_wp_cfg_dev_sel			_BV(0)
+#define dtm_wp_cfg_chn_sel_OFFSET		1
+#define dtm_wp_cfg_chn_sel_MASK			REG_3BIT_MASK
+#define dtm_wp_cfg_chn_sel(value)		_SET_FV_ULL(dtm_wp_cfg_chn_sel, value)
+#define dtm_wp_REQ				0
+#define dtm_wp_RSP				1
+#define dtm_wp_SNP				2
+#define dtm_wp_DAT				3
+#define dtm_wp_cfg_combine			_BV(6)
+#define dtm_wp_cfg_pkt_gen			_BV(8)
+#define dtm_wp_cfg_pkt_type_OFFSET		9
+#define dtm_wp_cfg_pkt_type_MASK		REG_3BIT_MASK
+#define dtm_wp_cfg_pkt_type(value)		_SET_FV_ULL(dtm_wp_cfg_pkt_type, value)
+#define dtm_wp_cfg_pkt_type_TXNID		0
+#define dtm_wp_cfg_pkt_type_TXNID_OP		1
+#define dtm_wp_cfg_pkt_type_TXNID_OP_SRC_TGT	2
+#define dtm_wp_cfg_pkt_type_CTRL_FLIT		4
+#define dtm_wp_cfg_cc_en			_BV(12)
+#define dtm_wp_cfg_ctrig_en			_BV(13)
+#define dtm_wp_cfg_dbgtrig_en			_BV(14)
 
-/* dtm_wp_SNP */
-#define dtm_wp_SNP_SRCID_OFFSET		0
-#define dtm_wp_SNP_SRCID_MASK		REG_11BIT_MASK
-#define dtm_wp_SNP_SRCID(value)		_SET_FV_ULL(dtm_wp_SNP_SRCID, value)
-#define dtm_wp_SNP_TGTID_OFFSET		0
-#define dtm_wp_SNP_TGTID_MASK		REG_11BIT_MASK
-#define dtm_wp_SNP_TGTID(value)		_SET_FV_ULL(dtm_wp_SNP_TGTID, value)
-#define dtm_wp_SNP_OPCODE_OFFSET	19
-#define dtm_wp_SNP_OPCODE_MASK		REG_5BIT_MASK
-#define dtm_wp_SNP_OPCODE(value)	_SET_FV_ULL(dtm_wp_SNP_OPCODE, value)
+/* dtm_wp_val & dtm_wp_mask*/
+/* REQ */
+#define dtm_wp_REQ_SRCID_OFFSET			0
+#define dtm_wp_REQ_SRCID_MASK			REG_11BIT_MASK
+#define dtm_wp_REQ_SRCID(value)			_SET_FV_ULL(dtm_wp_REQ_SRCID, value)
+#define dtm_wp_REQ_TGTID_OFFSET			0
+#define dtm_wp_REQ_TGTID_MASK			REG_11BIT_MASK
+#define dtm_wp_REQ_TGTID(value)			_SET_FV_ULL(dtm_wp_REQ_TGTID, value)
+#define dtm_wp_REQ_OPCODE_OFFSET		31
+#define dtm_wp_REQ_OPCODE_MASK			REG_6BIT_MASK
+#define dtm_wp_REQ_OPCODE(value)		_SET_FV_ULL(dtm_wp_REQ_OPCODE, value)
 
-/* dtm_wp_DAT */
-#define dtm_wp_DAT_SRCID_OFFSET		4
-#define dtm_wp_DAT_SRCID_MASK		REG_11BIT_MASK
-#define dtm_wp_DAT_SRCID(value)		_SET_FV_ULL(dtm_wp_DAT_SRCID, value)
-#define dtm_wp_DAT_TGTID_OFFSET		4
-#define dtm_wp_DAT_TGTID_MASK		REG_11BIT_MASK
-#define dtm_wp_DAT_TGTID(value)		_SET_FV_ULL(dtm_wp_DAT_TGTID, value)
-#define dtm_wp_DAT_OPCODE_OFFSET	26
-#define dtm_wp_DAT_OPCODE_MASK		REG_3BIT_MASK
-#define dtm_wp_DAT_OPCODE(value)	_SET_FV_ULL(dtm_wp_DAT_OPCODE, value)
+/* RSP */
+#define dtm_wp_RSP_SRCID_OFFSET			4
+#define dtm_wp_RSP_SRCID_MASK			REG_11BIT_MASK
+#define dtm_wp_RSP_SRCID(value)			_SET_FV_ULL(dtm_wp_RSP_SRCID, value)
+#define dtm_wp_RSP_TGTID_OFFSET			4
+#define dtm_wp_RSP_TGTID_MASK			REG_11BIT_MASK
+#define dtm_wp_RSP_TGTID(value)			_SET_FV_ULL(dtm_wp_RSP_TGTID, value)
+#define dtm_wp_RSP_OPCODE_OFFSET		15
+#define dtm_wp_RSP_OPCODE_MASK			REG_4BIT_MASK
+#define dtm_wp_RSP_OPCODE(value)		_SET_FV_ULL(dtm_wp_RSP_OPCODE, value)
+
+/* SNP */
+#define dtm_wp_SNP_SRCID_OFFSET			0
+#define dtm_wp_SNP_SRCID_MASK			REG_11BIT_MASK
+#define dtm_wp_SNP_SRCID(value)			_SET_FV_ULL(dtm_wp_SNP_SRCID, value)
+#define dtm_wp_SNP_TGTID_OFFSET			0
+#define dtm_wp_SNP_TGTID_MASK			REG_11BIT_MASK
+#define dtm_wp_SNP_TGTID(value)			_SET_FV_ULL(dtm_wp_SNP_TGTID, value)
+#define dtm_wp_SNP_OPCODE_OFFSET		19
+#define dtm_wp_SNP_OPCODE_MASK			REG_5BIT_MASK
+#define dtm_wp_SNP_OPCODE(value)		_SET_FV_ULL(dtm_wp_SNP_OPCODE, value)
+
+/* DAT */
+#define dtm_wp_DAT_SRCID_OFFSET			4
+#define dtm_wp_DAT_SRCID_MASK			REG_11BIT_MASK
+#define dtm_wp_DAT_SRCID(value)			_SET_FV_ULL(dtm_wp_DAT_SRCID, value)
+#define dtm_wp_DAT_TGTID_OFFSET			4
+#define dtm_wp_DAT_TGTID_MASK			REG_11BIT_MASK
+#define dtm_wp_DAT_TGTID(value)			_SET_FV_ULL(dtm_wp_DAT_TGTID, value)
+#define dtm_wp_DAT_OPCODE_OFFSET		26
+#define dtm_wp_DAT_OPCODE_MASK			REG_3BIT_MASK
+#define dtm_wp_DAT_OPCODE(value)		_SET_FV_ULL(dtm_wp_DAT_OPCODE, value)
+
+/* dt_dtc_ctl */
+#define dt_dtc_ctl_en				_BV(0)
+#define dt_dtc_ctl_dbgtrigger_en		_BV(1)
+#define dt_dtc_ctl_atbtrigger_en		_BV(2)
+#define dt_dtc_ctl_wait_for_trigger		_BV(3)
+
+/* dt_trace_control */
+#define dt_trace_control_cc_enable		_BV(8)
 
 #endif /* __CMN600_TRACE_H_INCLUDE__ */
