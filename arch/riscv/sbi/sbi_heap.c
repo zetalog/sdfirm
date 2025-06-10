ad2197ae0236f460e8af3c196e936e61ce27977e
Lv Zheng <zhenglv@hotmail.com>
2025-06-10 18:03:41 +0800

    sbi: Fix payload protection
    This patch fixes SBI heap support to allow S-mode to access
    payload and firmware tables.
    
    Signed-off-by: Lv Zheng <zhenglv@hotmail.com?

--- arch/riscv/sbi/sbi_heap.c
+++ arch/riscv/sbi/sbi_heap.c
@@ -3,18 +3,18 @@
 #include <target/heap.h>
 #include <target/paging.h>
 
-#define RESERVED_HEAP		8192
+extern uintptr_t __sbi_heap_start[];
+extern uintptr_t __sbi_heap_end[];
 
 int sbi_heap_init(struct sbi_scratch *scratch)
 {
         phys_addr_t pa_start = round_down(__pa(SDFIRM_START), PAGE_SIZE);
-        phys_addr_t pa_end = round_up(__pa(SDFIRM_END), PAGE_SIZE) + RESERVED_HEAP;
+        phys_addr_t pa_end = round_up(__pa(SDFIRM_END), PAGE_SIZE);
 
         mem_hw_range_init();
         mem_reserve(pa_start, pa_end - pa_start);
-        mem_reserve(pa_end, round_up(CONFIG_HEAP_SIZE, PAGE_SIZE));
 
-	heap_range_init(pa_end);
+	heap_range_init((caddr_t)__sbi_heap_start);
 	heap_init();
 	return SBI_OK;
 }
